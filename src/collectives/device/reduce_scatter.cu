5782a8d857fafb2c71b0492bb46a57ab534c1370
Wenkai Du <wenkai.du@amd.com>
2019-08-14 13:22:43 -0700

    Remove duplicate line

--- src/collectives/device/reduce_scatter.cu
+++ src/collectives/device/reduce_scatter.cu
@@ -1,7 +1,6 @@
 /*************************************************************************
  * Copyright (c) 2015-2018, NVIDIA CORPORATION. All rights reserved.
  * Modifications Copyright (c) 2019 Advanced Micro Devices, Inc. All rights reserved.
- * Modifications Copyright (c) 2019 Advanced Micro Devices, Inc. All rights reserved.
  *
  * See LICENSE.txt for license information
  ************************************************************************/
