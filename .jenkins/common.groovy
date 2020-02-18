edb863de6243dfe4af39f520471d7058001952fe
Eiden Yoshida <47196116+eidenyoshida@users.noreply.github.com>
2020-02-18 11:25:29 -0700

    Refactor Jenkinsfiles to allow use of new docker containers (#170)

---
+++ .jenkins/common.groovy
@@ -0,0 +1,32 @@
+// This file is for internal AMD use.
+// If you are interested in running your own Jenkins, please raise a github issue for assistance.
+
+def runCompileCommand(platform, project, jobName)
+{
+    project.paths.construct_build_prefix()
+
+    String hipclangArgs = jobName.contains('hipclang') ? '--hip-clang' : ''
+    def command = """#!/usr/bin/env bash
+                set -x
+                cd ${project.paths.project_build_prefix}
+                LD_LIBRARY_PATH=/opt/rocm/hcc/lib CXX= ${project.paths.build_command} -t {hipclangArgs}
+            """
+
+    platform.runCommand(this,command)
+}
+
+
+def runPackageCommand(platform, project, jobName)
+{
+    def command = """
+                      set -x
+                      cd ${project.paths.project_build_prefix}/build/release
+                      make package
+                      rm -rf package && mkdir -p package
+                      mv *.deb package/
+                      """
+	platform.runCommand(this,command)
+    platform.archiveArtifacts(this, """${project.paths.project_build_prefix}/build/release/package/*.deb""")
+}
+
+return this
