2d74402c0b71a09ada33133a7ea339519e833c58
Wenkai Du <wenkai.du@amd.com>
2020-08-15 11:46:58 -0700

    Fine tune one Rome model

--- src/graph/search.cc
+++ src/graph/search.cc
@@ -879,7 +879,7 @@ static bool validate4P1H(struct ncclTopoSystem* system, int *hive) {
 
 static ncclResult_t parseRome4P2H(struct ncclTopoSystem* system, char **str) {
   static const char *ringBase_10302120_1 = "7 4 5 3 1 0 6 2|4 7 3 5 0 1 2 6";
-  static const char *ringBase_10302120_2 = "7 6 5 4 3 0 1 2|6 7 4 5 0 3 2 1";
+  static const char *ringBase_10302120_2 = "6 4 7 5 0 1 3 2|6 5 7 4 2 3 1 0";
   static const char *ringBase_11303011_1 = "2 1 0 3 6 7 5 4|7 6 4 5 1 2 3 0";
   static const char *ringBase_11303011_2 = "0 6 2 3 1 7 5 4|7 1 4 5 6 0 3 2";
   static const char *ringBase;
