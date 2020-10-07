08bf3e85aebc12d95a66bb000bfe311349f02e2d
Gilbert Lee <gilbert.lee@amd.com>
2020-10-07 06:02:55 +0000

    Adding copywrite headers and fixing autounroll

--- tools/HelloRccl/HelloRccl.cpp
+++ tools/HelloRccl/HelloRccl.cpp
@@ -1,4 +1,25 @@
-#include "rccl.h"
+/*
+Copyright (c) 2020 Advanced Micro Devices, Inc. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
 #include <sys/socket.h>
 #include <ifaddrs.h>
 #include <netdb.h>
@@ -6,7 +27,10 @@
 #include <cstdio>
 #include <string>
 #include <chrono>
+#include <hip/hip_runtime.h>
+#include <rccl.h>
 #include "HelloRccl.hpp"
+
 
 void Usage(char *argv0);
 
