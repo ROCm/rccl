e8177c9ee78e4743bd1b3aa5af26e586d005e8a5
saadrahim <44449863+saadrahim@users.noreply.github.com>
2020-10-20 09:39:03 -0600

    Adding sles15, centos7 and centos8 testing (#283)

--- .jenkins/common.groovy
+++ .jenkins/common.groovy
@@ -21,7 +21,7 @@ def runTestCommand (platform, project)
     def command = """#!/usr/bin/env bash
                 set -x
                 cd ${project.paths.project_build_prefix}/build/release/test
-                NCCL_DEBUG=INFO HSA_FORCE_FINE_GRAIN_PCIE=1 ./UnitTests --gtest_output=xml --gtest_color=yes
+                ${sudo} NCCL_DEBUG=INFO HSA_FORCE_FINE_GRAIN_PCIE=1 ./UnitTests --gtest_output=xml --gtest_color=yes
             """
 
    platform.runCommand(this, command)
