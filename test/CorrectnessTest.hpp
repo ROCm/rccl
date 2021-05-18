9fbe4600d262e890330226b5a9e6e255a1cab78d
Gilbert Lee <gilbert.lee@amd.com>
2021-05-18 17:55:09 +0000

    Disabling env var caching for all unit tests

--- test/CorrectnessTest.hpp
+++ test/CorrectnessTest.hpp
@@ -553,9 +553,10 @@ dropback:
 
             envString = 0;
             numTokens = 0;
+            setenv("RCCL_TEST_ENV_VARS", "ENABLE", 1);
             if (strcmp(envVals, "")) {
                 // enable RCCL env vars testing
-                setenv("RCCL_TEST_ENV_VARS", "ENABLE", 1);
+
                 envString = strdup(envVals);
                 tokens[numTokens] = strtok(envString, "=, ");
                 numTokens++;
