10f3534e4a444f32681a23c0d6a09aeed4c3f2cd
Wenkai Du <43822138+wenkaidu@users.noreply.github.com>
2021-06-09 15:26:00 -0700

    Add support for another Rome model (#385) (#388)
    (cherry picked from commit c2064adcc7c26b5cd56477dc71c417f1418465c7)

--- src/graph/rome_models.cc
+++ src/graph/rome_models.cc
@@ -275,6 +275,17 @@ static struct rcclRomeModel rome_model_52 = {
   .ringBase = "0 1 3 2 4 5 7 6|6 7 5 4 2 3 1 0|0 1 5 4 6 7 3 2|2 3 7 6 4 5 1 0",
 };
 
+static struct rcclRomeModel rome_model_43 = {
+  .nGpus = 8, .nCpus = 4, .nNics = 0, .nLinks = 3,
+  .gpuIds = { 0x63000, 0x43000, 0x27000, 0x3000, 0xe3000, 0xc3000, 0xa3000, 0x83000, },
+  .nicIds = { },
+  .gpuNuma = { 0, 0, 1, 1, 2, 2, 3, 3, },
+  .nicNuma = { },
+  .connMatrix = { 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, },
+  .pattern = "20202020",
+  .ringBase = "0 1 2 3 4 5 6 7|0 2 5 7 4 6 1 3|0 3 1 6 4 7 5 2|0 7 6 5 4 3 2 1",
+};
+
 static struct rcclRomeModel romeTopoModels[] = {
   rome_model_22,
   rome_model_25,
@@ -297,6 +308,7 @@ static struct rcclRomeModel romeTopoModels[] = {
   rome_model_48,
   rome_model_49,
   rome_model_52,
+  rome_model_43,
 };
 
 /* Parse user defined rings. Format is like :
