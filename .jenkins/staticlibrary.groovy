d65ef43bb4404dec68c18ad4ab2624fac8e5b1a4
Eiden Yoshida <eiden.yoshida@amd.com>
2021-06-11 17:41:38 -0600

    Add test filter for test stage

--- .jenkins/staticlibrary.groovy
+++ .jenkins/staticlibrary.groovy
@@ -31,7 +31,7 @@ def runCI =
     {
         platform, project->
 
-        commonGroovy.runTestCommand(platform, project)
+        commonGroovy.runTestCommand(platform, project, "*sum_float32*")
     }
 
     def packageCommand =
