6d3c9a7d48321b56b36b6b818fa84811e1cd1385
Wenkai Du <wenkai.du@amd.com>
2021-09-10 16:20:21 -0700

    atomicAdd in ROCm 4.3 only support unsigned long long

--- src/collectives/device/primitives.h
+++ src/collectives/device/primitives.h
@@ -40,8 +40,8 @@
     const int wid = threadIdx.x%WARP_SIZE; \
     if (wid == 0) { \
       barrier_next[w] += nthreads/WARP_SIZE; \
-      atomicAdd(barriers, 1); \
-      while (atomicAdd(barriers, 0) < barrier_next[w]) __builtin_amdgcn_s_sleep(64); \
+      atomicAdd((unsigned long long *)barriers, 1); \
+      while (atomicAdd((unsigned long long *)barriers, 0) < barrier_next[w]) __builtin_amdgcn_s_sleep(64); \
       __asm__ __volatile__("s_wakeup"); \
     } \
   } \
