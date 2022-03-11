2e1d1cddd7f807586769c9a10613b362e35dd744
Liam Wrubleski <Liam.Wrubleski@amd.com>
2022-03-11 13:38:47 -0700

    Use static googletest

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -52,7 +52,7 @@ if(NOT GTest_FOUND AND BUILD_TESTS OR INSTALL_DEPENDENCIES)
                      GIT_REPOSITORY      https://github.com/google/googletest.git
                      GIT_TAG             release-1.11.0
                      INSTALL_DIR         ${GTEST_ROOT}
-                     CMAKE_ARGS          -DBUILD_GTEST=ON -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR> ${COMPILER_OVERRIDE}
+                     CMAKE_ARGS          -DBUILD_GTEST=ON -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR> ${COMPILER_OVERRIDE} -DBUILD_SHARED_LIBS=OFF
                      LOG_DOWNLOAD        TRUE
                      LOG_CONFIGURE       TRUE
                      LOG_BUILD           TRUE
