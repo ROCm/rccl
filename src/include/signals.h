ad89af4df1e6b62bab676943ce9f33061be9d449
Gilbert Lee <gilbert.lee@amd.com>
2022-05-19 21:41:02 +0000

    Moving opt-in custom signal handler from UnitTests into RCCL

---
+++ src/include/signals.h
@@ -0,0 +1,12 @@
+/*************************************************************************
+ * Copyright (c) 2022 Advanced Micro Devices, Inc. All rights reserved.
+ *
+ * See LICENSE.txt for license information
+ ************************************************************************/
+
+#ifndef RCCL_SIGNALS_H_
+#define RCCL_SIGNALS_H_
+
+void RegisterSignalHandlers();
+
+#endif
