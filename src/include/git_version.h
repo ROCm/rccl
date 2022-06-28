b6309762f2bd046a45bd9c62cc0c5b9231635086
Gilbert Lee <gilbert.lee@amd.com>
2022-06-28 21:01:04 +0000

    Adding git hash info to version output line

---
+++ src/include/git_version.h
@@ -0,0 +1,12 @@
+/*************************************************************************
+ * Copyright (c) 2022 Advanced Micro Devices, Inc. All rights reserved.
+ *
+ * See LICENSE.txt for license information
+ ************************************************************************/
+
+#ifndef RCCL_GIT_VERSION_H_
+#define RCCL_GIT_VERSION_H_
+
+extern const char *rcclGitHash;
+
+#endif
