9f3282dd098274f41e5033d8d8f6fcef9dcdfa84
akolliasAMD <akollias@amd.com>
2022-08-25 19:44:52 +0000

    renamed everything to Cur git version

--- cmake/git_version.cmake
+++ cmake/git_version.cmake
@@ -5,7 +5,7 @@ execute_process(COMMAND git log --pretty=format:'%h' -n 1
 
 # Check if git information was found
 if ("${GIT_REV}" STREQUAL "")
-  set(GIT_VERSION "const char *rcclGitHash =\"Unknown \";")
+  set(CURR_GIT_VERSION "const char *rcclGitHash =\"Unknown \";")
 else()
   # Check for changes (denote with a '+') after hash
   execute_process(
@@ -21,22 +21,22 @@ else()
   string(STRIP "${GIT_DIFF}" GIT_DIFF)
   string(STRIP "${GIT_BRANCH}" GIT_BRANCH)
 
-  set(GIT_VERSION "const char *rcclGitHash =\"${GIT_BRANCH}:${GIT_REV}${GIT_DIFF}\";")
+  set(CURR_GIT_VERSION "const char *rcclGitHash =\"${GIT_BRANCH}:${GIT_REV}${GIT_DIFF}\";")
 endif()
 
 # Compare file with older git version file (git_version.cpp)
 if (EXISTS ${CMAKE_CURRENT_BINARY_DIR}/git_version.cpp)
   #MESSAGE(STATUS "Found ${CMAKE_CURRENT_BINARY_DIR}/git_version.cpp")
   file(READ ${CMAKE_CURRENT_BINARY_DIR}/git_version.cpp PREV_GIT_VERSION)
-  #message(STATUS "CURR GIT version: ${GIT_VERSION}")
+  #message(STATUS "CURR GIT version: ${CURR_GIT_VERSION}")
   #message(STATUS "PREV GIT version: ${PREV_GIT_VERSION}")
-  if (NOT "${GIT_VERSION}" STREQUAL "${PREV_GIT_VERSION}")
+  if (NOT "${CURR_GIT_VERSION}" STREQUAL "${PREV_GIT_VERSION}")
     message(STATUS "Updating git_version.cpp")
-    file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/git_version.cpp "${GIT_VERSION}")
+    file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/git_version.cpp "${CURR_GIT_VERSION}")
   else()
     message(STATUS "No changes to git_version.cpp required")
   endif()
 else()
   # Create git_version.cpp if it doesn't exist yet
-  file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/git_version.cpp "${GIT_VERSION}")
+  file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/git_version.cpp "${CURR_GIT_VERSION}")
 endif()
