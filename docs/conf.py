5ae0dc2016859af31b37cae742e3e8f76b25c5eb
Saad Rahim <44449863+saadrahim@users.noreply.github.com>
2023-03-13 11:00:57 -0600

    Documentation update for Read the Docs
    Standard template implementation (#703)
    
    Fix Docs static analysis (#708)
    
    * remove ref to old script in static analysis
    
    * update README with doc build instructions
    
    update gitignore with doc artifacts
    
    pin rocm-docs-core and add dependabot config (#722)
    
    Bump rocm-docs-core from 0.2.0 to 0.5.0 in /docs/.sphinx (#728)
    
    Bumps [rocm-docs-core](https://github.com/RadeonOpenCompute/rocm-docs-core) from 0.2.0 to 0.5.0.
    - [Release notes](https://github.com/RadeonOpenCompute/rocm-docs-core/releases)
    - [Changelog](https://github.com/RadeonOpenCompute/rocm-docs-core/blob/develop/CHANGELOG.md)
    - [Commits](https://github.com/RadeonOpenCompute/rocm-docs-core/commits/v0.5.0)
    
    ---
    updated-dependencies:
    - dependency-name: rocm-docs-core
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    Update Read the Docs, documentation, and dependabot (#772)
    
    * update documentation
    
    add version number to documentation
    
    rename .sphinx/.doxygen to sphinx/doxygen
    
    enable htmlzip, pdf, epub formats when publishing on Read the Docs
    
    * add noCI label for dependabot PRs
    
    since RTD CI is separate from math lib CI
    
    * update rocm-docs-core to v0.13.4
    
    * update README with link to rocm.docs.amd.com

---
+++ docs/conf.py
@@ -0,0 +1,27 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import subprocess
+
+from rocm_docs import ROCmDocs
+
+
+name = "RCCL"
+get_major = r'sed -n -e "s/^NCCL_MAJOR.*\([0-9]\+\).*/\1/p" ../makefiles/version.mk'
+get_minor = r'sed -n -e "s/^NCCL_MINOR.*\([0-9]\{2,\}\).*/\1/p" ../makefiles/version.mk'
+get_patch = r'sed -n -e "s/^NCCL_PATCH.*\([0-9]\+\).*/\1/p" ../makefiles/version.mk'
+major = subprocess.getoutput(get_major)
+minor = subprocess.getoutput(get_minor)
+patch = subprocess.getoutput(get_patch)
+
+external_toc_path = "./sphinx/_toc.yml"
+
+docs_core = ROCmDocs(f"{name} {major}.{minor}.{patch} Documentation")
+docs_core.run_doxygen(doxygen_root="doxygen", doxygen_path="doxygen/docBin/xml")
+docs_core.setup()
+
+for sphinx_var in ROCmDocs.SPHINX_VARS:
+    globals()[sphinx_var] = getattr(docs_core, sphinx_var)
