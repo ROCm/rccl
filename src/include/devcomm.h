b21383292403951258d0739b222ab25414c79b47
Cordell Bloor <Cordell.Bloor@amd.com>
2023-03-26 19:14:09 -0600

    Fix build on additional architectures
    Instead of directly wrapping a platform-specific operation with a
    preprocessor check against a gfx macro, it can be more flexible to
    check a macro that can be overriden by the user. The gfx macro can then
    just provide the default value for the macro, resulting in the same
    default behaviour as if the gfx macro was checked directly but with
    more control at build-time.
    
    For example, to build rccl without using buffer_wbinvl1_vol on
    gfx902, but still use the default on other archs, a user could
    export CXXFLAGS='-Xarch_gfx902 -DRCCL_USE_WBINVL1_VOL=1' before
    configuring the build. This flexibility isn't always necessary, but
    it's nicer to have it and not need it than to need it and not have it.

--- src/include/devcomm.h
+++ src/include/devcomm.h
@@ -53,7 +53,7 @@ union ncclLLFifoLine {
   int4 i4;
 };
 
-#if defined(__gfx1030__)  || defined(__gfx1100__) || defined(__gfx1101__) || defined(__gfx1102__)
+#if defined(__GFX10__) || defined(__GFX11__)
 #define WARP_SIZE 32
 #else
 #define WARP_SIZE 64
