ad5d6a5e0016f9b48de3ac4ac926d5f9df7f9e30
Wenkai Du <wenkai.du@amd.com>
2023-07-13 11:33:44 -0500

    Add default ring configuration for gfx940

--- tools/rccl-prim-test/rccl_prim_test.cpp
+++ tools/rccl-prim-test/rccl_prim_test.cpp
@@ -426,14 +426,23 @@ int main(int argc,char* argv[])
   static const char *ring_4p3l = "0 1 2 3|0 1 3 2|0 2 1 3|0 2 3 1|0 3 1 2|0 3 2 1";
   static const char *ring_8p1h = "0 1 3 2 4 5 7 6|6 7 5 4 2 3 1 0|0 1 5 4 6 7 3 2|2 3 7 6 4 5 1 0";
   static const char *ring_16p1h = "0 1 3 2 6 7 15 14 10 11 9 8 12 13 5 4|0 1 2 3 7 6 13 12 8 9 10 11 15 14 5 4|0 2 3 7 6 14 15 11 10 8 9 13 12 4 5 1|4 5 13 12 8 9 11 10 14 15 7 6 2 3 1 0|4 5 14 15 11 10 9 8 12 13 6 7 3 2 1 0|1 5 4 12 13 9 8 10 11 15 14 6 7 3 2 0";
+  static const char *ring_gfx940_8p = "0 1 2 3 4 5 6 7|0 1 2 3 4 5 7 6|0 2 4 1 3 6 5 7|0 2 4 6 1 7 3 5|0 3 1 5 2 7 4 6|0 3 5 1 6 2 7 4|0 4 1 7 3 6 2 5|7 6 5 4 3 2 1 0|6 7 5 4 3 2 1 0|7 5 6 3 1 4 2 0|5 3 7 1 6 4 2 0|6 4 7 2 5 1 3 0|4 7 2 6 1 5 3 0|5 2 6 3 7 1 4 0";
+
   setupPeers(connection_info, &is_xgmi);
   if (!r) {
     parseChordalRing(&cr8g);
     if (nGpu == 4 && is_xgmi) r = (char *)ring_4p3l;
     if (nGpu == 8 && cr8g) r = (char *)cr8g;
     if (nGpu == 8 && !cr8g) {
-      r = (char *)ring_8p1h;
-      if(!workgroups) workgroups = 16;
+      hipDeviceProp_t prop;
+      HIPCHECK(hipGetDeviceProperties(&prop, 0));
+      if (prop.gcnArch/10 == 94) {
+        r = (char *)ring_gfx940_8p;
+        if(!workgroups) workgroups = 28;
+      } else {
+        r = (char *)ring_8p1h;
+        if(!workgroups) workgroups = 16;
+      }
     }
     if (nGpu == 16) {
       r = (char *)ring_16p1h;
