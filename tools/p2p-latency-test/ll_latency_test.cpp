1c1ec096e24b019e559902847802245eca9f80a9
Wenkai Du <43822138+wenkaidu@users.noreply.github.com>
2023-07-25 20:08:04 -0700

    tools: Add LL latency test (#820)
    * Add LL latency test
    
    * Correct name in usage

--- tools/p2p-latency-test/ll_latency_test.cpp
+++ tools/p2p-latency-test/ll_latency_test.cpp
@@ -1,5 +1,6 @@
 /*************************************************************************
  * Copyright (c) Microsoft Corporation.
+ * Modifications Copyright (c) 2023 Advanced Micro Devices, Inc. All rights reserved.
  * Licensed under the MIT License.
  ************************************************************************/
 
@@ -19,33 +20,92 @@
 #define PING_MODE 0
 #define PONG_MODE 1
 
-__global__ void PingKernel(uint64_t* local_flag, uint64_t* remote_flag, uint64_t* time_delta) {
+#define LL_MAX_THREAD 256
+
+union LLFifoLine {
+  /* Flags have to be *after* data, because otherwise, an incomplete receive
+     from the network may receive the flag but not the data.
+     Note this is assuming that either we receive contiguous chunks of data
+     (sockets) or data is written with an atomicity of 8 bytes (IB/RDMA). */
+  struct {
+    uint32_t data1;
+    uint32_t flag1;
+    uint32_t data2;
+    uint32_t flag2;
+  };
+  uint64_t v[2];
+  int4 i4;
+};
+
+__device__ void storeLL(union LLFifoLine* dst, uint64_t val, uint32_t flag) {
+  union LLFifoLine i4;
+  i4.data1 = val & 0xffffffff;
+  i4.flag1 = flag;
+  i4.data2 = (val >> 32);
+  i4.flag2 = flag;
+  __builtin_nontemporal_store(i4.v[0], dst->v);
+  __builtin_nontemporal_store(i4.v[1], dst->v+1);
+}
+
+#define LL_SPINS_BEFORE_CHECK_ABORT 1000000
+
+__device__ uint32_t *abortFlag;
+
+inline __device__ int checkAbort(int &spins) {
+  uint32_t abort = 0;
+  spins++;
+  if (spins == LL_SPINS_BEFORE_CHECK_ABORT) {
+    abort = __atomic_load_n(abortFlag, __ATOMIC_SEQ_CST);
+    spins = 0;
+  }
+  return abort;
+}
+
+__device__ uint64_t readLL(union LLFifoLine* src, uint32_t flag) {
+  uint32_t data1, flag1, data2, flag2;
+  int spins = 0;
+
+  union LLFifoLine i4;
+  do {
+    i4.v[0] = __builtin_nontemporal_load(src->v);
+    i4.v[1] = __builtin_nontemporal_load(src->v+1);
+    if (checkAbort(spins)) break;
+  } while ((i4.flag1 != flag) || (i4.flag2 != flag));
+  uint64_t val64 = (uint64_t)(i4.data1) + (((uint64_t)i4.data2) << 32);
+
+  return val64;
+}
+
+
+__global__ void PingKernel(LLFifoLine* local_flag, LLFifoLine* remote_flag, uint64_t* time_delta) {
+  int tid = threadIdx.x;
   #pragma unroll
   for (uint32_t i = 1; i < NUM_LOOPS_WARMUP; i++) {
-    __atomic_store_n(remote_flag, i, __ATOMIC_RELAXED);
-    while (__atomic_load_n(local_flag, __ATOMIC_RELAXED) != i);
+    storeLL(remote_flag+tid, i, i);
+    while (readLL(local_flag+tid, i) != i);
   }
   uint64_t start_time = wall_clock64();
   #pragma unroll
   for (uint32_t i = NUM_LOOPS_WARMUP; i <= NUM_LOOPS_WARMUP + NUM_LOOPS_RUN; i++) {
-    __atomic_store_n(remote_flag, i, __ATOMIC_RELAXED);
-    while (__atomic_load_n(local_flag, __ATOMIC_RELAXED) != i);
+    storeLL(remote_flag+tid, i, i);
+    while (readLL(local_flag+tid, i) != i);
   }
   uint64_t end_time = wall_clock64();
   *time_delta = end_time - start_time;
 }
 
-__global__ void PongKernel(uint64_t* local_flag, uint64_t* remote_flag, uint64_t* time_delta) {
+__global__ void PongKernel(LLFifoLine* local_flag, LLFifoLine* remote_flag, uint64_t* time_delta) {
+  int tid = threadIdx.x;
   #pragma unroll
   for (uint32_t i = 1; i < NUM_LOOPS_WARMUP; i++) {
-    while (__atomic_load_n(local_flag, __ATOMIC_RELAXED) != i);
-    __atomic_store_n(remote_flag, i, __ATOMIC_RELAXED);
+    while (readLL(local_flag+tid, i) != i);
+    storeLL(remote_flag+tid, i, i);
   }
   uint64_t start_time = wall_clock64();
   #pragma unroll
   for (uint32_t i = NUM_LOOPS_WARMUP; i <= NUM_LOOPS_WARMUP + NUM_LOOPS_RUN; i++) {
-    while (__atomic_load_n(local_flag, __ATOMIC_RELAXED) != i);
-    __atomic_store_n(remote_flag, i, __ATOMIC_RELAXED);
+    while (readLL(local_flag+tid, i) != i);
+    storeLL(remote_flag+tid, i, i);
   }
   uint64_t end_time = wall_clock64();
   *time_delta = end_time - start_time;
@@ -56,7 +116,7 @@ int main(int argc, char** argv) {
   hipError_t err = hipSuccess;
 
   if (argc != 2) {
-    fprintf(stderr, "Usage: ./ll_latency_test <flag>; flag=%d for ping mode, flag=%d for pong mode\n", PING_MODE, PONG_MODE);
+    fprintf(stderr, "Usage: ./p2p_latency_test <flag>; flag=%d for ping mode, flag=%d for pong mode\n", PING_MODE, PONG_MODE);
     return -1;
   }
 
@@ -65,8 +125,8 @@ int main(int argc, char** argv) {
   hipGetDevice(&device_id);
   hipGetDeviceProperties(&prop, device_id);
 
-  uint64_t *local_flag = nullptr;
-  uint64_t *remote_flag = nullptr;
+  LLFifoLine *local_flag = nullptr;
+  LLFifoLine *remote_flag = nullptr;
   uint64_t *time_delta = nullptr;
   hipStreamCreateWithFlags(&stream, hipStreamNonBlocking);
   hipExtMallocWithFlags((void**)&local_flag, HIP_IPC_MEM_MIN_SIZE, prop.gcnArch / 10 == 94 ? hipDeviceMallocUncached : hipDeviceMallocFinegrained);
@@ -97,9 +157,9 @@ int main(int argc, char** argv) {
       return -1;
     }
     if (self_mode == PING_MODE) {
-      PingKernel<<<1, 1, 0, stream>>>(local_flag, remote_flag, time_delta);
+      PingKernel<<<1, LL_MAX_THREAD, 0, stream>>>(local_flag, remote_flag, time_delta);
     } else {
-      PongKernel<<<1, 1, 0, stream>>>(local_flag, remote_flag, time_delta);
+      PongKernel<<<1, LL_MAX_THREAD, 0, stream>>>(local_flag, remote_flag, time_delta);
     }
     err = hipStreamSynchronize(stream);
     if (err != hipSuccess) {
