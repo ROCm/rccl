3db371c9a502b0288415aa9f2c2b4368594ec3cf
Wenkai Du <43822138+wenkaidu@users.noreply.github.com>
2023-07-27 20:25:18 -0700

    Revert "Enable Ll128 on gfx90a (#823)" (#829)
    This reverts commit 420f8af6a0f167a4b062b45fd3e5ef607c96b965.
    
    Also increase number of parallel jobs for linking

--- src/collectives/device/common.h
+++ src/collectives/device/common.h
@@ -28,7 +28,7 @@
   { __atomic_store_n((DST), (SRC), __ATOMIC_SEQ_CST); }
 #endif
 
-#ifdef __gfx90a__
+#ifdef ENABLE_LL128
 #define NCCL_FUNC5(func, algo, devredop, type, nullify) \
   MACRO_IF(nullify, nullptr, NCCL_FUNC_NAME(func, algo, LL,     devredop, type)), \
   MACRO_IF(nullify, nullptr, NCCL_FUNC_NAME(func, algo, LL128,  devredop, type)), \
@@ -549,7 +549,7 @@ __forceinline__ __device__ void ncclKernel(
 #ifdef USE_INDIRECT_FUNCTION_CALL
       ncclFuncs[ncclShmem.work.header.funcIndex]();
 #else
-#ifdef __gfx90a__
+#ifdef ENABLE_LL128
       NCCL_CALL_FUNCTIONS<1>(ncclShmem.work.header.funcIndex);
 #else
       NCCL_CALL_FUNCTIONS<0>(ncclShmem.work.header.funcIndex);
@@ -618,7 +618,7 @@ __device__  __attribute__((noinline)) void NCCL_FUNC_NAME(func, algo, proto, dev
 #endif
 
 // Only generate inline kernels for LL
-#ifdef __gfx90a__
+#ifdef ENABLE_LL128
 #define IMPL_COLL4(func, algo, devredop, type) \
   IMPL_COLL_FUNC(func, algo, LL,     devredop, type) \
   IMPL_COLL_FUNC(func, algo, LL128,  devredop, type) \
