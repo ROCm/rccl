c6dd6f623798fd9f2995fb1c5e7e4eeca35dd0c9
Wenkai Du <43822138+wenkaidu@users.noreply.github.com>
2023-08-30 16:29:42 -0700

    Update ll_latency_test and add CUDA version (#873)

--- tools/p2p-latency-test/ll_latency_test.cu
+++ tools/p2p-latency-test/ll_latency_test.cu
@@ -10,7 +10,7 @@
 #include <fstream>
 #include <string>
 #include <unistd.h>
-#include <hip/hip_runtime.h>
+#include <cuda_runtime.h>
 #include <iostream> //cerr
 #include <cstring>
 
@@ -38,13 +38,7 @@ union LLFifoLine {
 #define HIP_IPC_MEM_MIN_SIZE (LL_MAX_THREADS*LL_MAX_LINES*sizeof(LLFifoLine))
 
 __device__ void storeLL(union LLFifoLine* dst, uint64_t val, uint32_t flag) {
-  union LLFifoLine i4;
-  i4.data1 = val & 0xffffffff;
-  i4.flag1 = flag;
-  i4.data2 = (val >> 32);
-  i4.flag2 = flag;
-  __builtin_nontemporal_store(i4.v[0], dst->v);
-  __builtin_nontemporal_store(i4.v[1], dst->v+1);
+  asm volatile("st.volatile.global.v4.u32 [%0], {%1,%2,%3,%4};" :: "l"(&dst->i4), "r"((uint32_t)val), "r"(flag), "r"((uint32_t)(val >> 32)), "r"(flag));
 }
 
 #define LL_SPINS_BEFORE_CHECK_ABORT 1000000
@@ -53,24 +47,21 @@ inline __device__ int checkAbort(int &spins, uint32_t* abortFlag) {
   uint32_t abort = 0;
   spins++;
   if (spins == LL_SPINS_BEFORE_CHECK_ABORT) {
-    abort = __atomic_load_n(abortFlag, __ATOMIC_SEQ_CST);
+    abort = *((volatile uint32_t*)abortFlag);
     spins = 0;
   }
   return abort;
 }
 
 __device__ uint64_t readLL(union LLFifoLine* src, uint32_t flag, uint32_t* abortFlag) {
-  uint32_t data1, flag1, data2, flag2;
   int spins = 0;
 
   union LLFifoLine i4;
   do {
-    i4.v[0] = __builtin_nontemporal_load(src->v);
-    i4.v[1] = __builtin_nontemporal_load(src->v+1);
-    if (checkAbort(spins, abortFlag)) break;
+      asm("ld.volatile.global.v4.u32 {%0,%1,%2,%3}, [%4];" : "=r"(i4.data1), "=r"(i4.flag1), "=r"(i4.data2), "=r"(i4.flag2) : "l"(&src->i4));
+      if (checkAbort(spins, abortFlag)) break;
   } while ((i4.flag1 != flag) || (i4.flag2 != flag));
   uint64_t val64 = (uint64_t)(i4.data1) + (((uint64_t)i4.data2) << 32);
-
   return val64;
 }
 
@@ -83,14 +74,14 @@ __global__ void PingKernel(LLFifoLine* local_flag, LLFifoLine* remote_flag, uint
     while (readLL(local_flag+tid+(i%LL_MAX_LINES)*LL_MAX_THREADS, i, abortFlag) != i);
   }
   uint64_t start_time, end_time;
-  if (tid == 0) start_time = wall_clock64();
+  if (tid == 0) start_time = clock64();
   #pragma unroll
   for (uint32_t i = NUM_LOOPS_WARMUP; i <= NUM_LOOPS_WARMUP + NUM_LOOPS_RUN; i++) {
     storeLL(remote_flag+tid+(i%LL_MAX_LINES)*LL_MAX_THREADS, i, i);
     while (readLL(local_flag+tid+(i%LL_MAX_LINES)*LL_MAX_THREADS, i, abortFlag) != i);
   }
   __syncthreads();
-  if (tid == 0) end_time = wall_clock64();
+  if (tid == 0) end_time = clock64();
   if (tid == 0) *time_delta = end_time - start_time;
 }
 
@@ -102,21 +93,21 @@ __global__ void PongKernel(LLFifoLine* local_flag, LLFifoLine* remote_flag, uint
     storeLL(remote_flag+tid+(i%LL_MAX_LINES)*LL_MAX_THREADS, i, i);
   }
   uint64_t start_time, end_time;
-  if (tid == 0) start_time = wall_clock64();
+  if (tid == 0) start_time = clock64();
   #pragma unroll
   for (uint32_t i = NUM_LOOPS_WARMUP; i <= NUM_LOOPS_WARMUP + NUM_LOOPS_RUN; i++) {
     while (readLL(local_flag+tid+(i%LL_MAX_LINES)*LL_MAX_THREADS, i, abortFlag) != i);
     storeLL(remote_flag+tid+(i%LL_MAX_LINES)*LL_MAX_THREADS, i, i);
   }
   __syncthreads();
-  if (tid == 0) end_time = wall_clock64();
+  if (tid == 0) end_time = clock64();
   if (tid == 0) *time_delta = end_time - start_time;
 }
 
 #define HIPCHECK(cmd)                                                          \
 do {                                                                           \
-  hipError_t error = (cmd);                                                    \
-  if (error != hipSuccess)                                                     \
+  cudaError_t error = (cmd);                                                    \
+  if (error != cudaSuccess)                                                     \
   {                                                                            \
     std::cerr << "Encountered HIP error (" << error << ") at line "            \
               << __LINE__ << " in file " << __FILE__ << "\n";                  \
@@ -125,10 +116,9 @@ do {                                                                           \
 } while (0)
 
 int main(int argc, char** argv) {
-  hipStream_t stream[2];
-  hipError_t err = hipSuccess;
+  cudaStream_t stream[2];
   int device_id[2];
-  hipDeviceProp_t prop[2];
+  cudaDeviceProp prop[2];
 
   if (argc != 3) {
     fprintf(stderr, "Usage: ./ll_latency_test ping_dev_id pong_dev_id\n");
@@ -143,49 +133,49 @@ int main(int argc, char** argv) {
   uint64_t *time_delta[2];
   uint32_t *abortFlag[2];
 
-  HIPCHECK(hipSetDevice(device_id[0]));
-  HIPCHECK(hipStreamCreateWithFlags(&stream[0], hipStreamNonBlocking));
-  HIPCHECK(hipDeviceEnablePeerAccess(device_id[1], 0));
-  HIPCHECK(hipGetDeviceProperties(&prop[0], device_id[0]));
-  HIPCHECK(hipExtMallocWithFlags((void**)&flag[0], HIP_IPC_MEM_MIN_SIZE, prop[0].gcnArch / 10 == 94 ? hipDeviceMallocUncached : hipDeviceMallocFinegrained));
-  HIPCHECK(hipHostMalloc ((void**)&time_delta[0], sizeof(uint64_t), hipHostMallocDefault));
-  HIPCHECK(hipMalloc((void**)&abortFlag[0], sizeof(uint32_t)));
-  HIPCHECK(hipMemsetAsync(flag[0], 0, HIP_IPC_MEM_MIN_SIZE, stream[0]));
-  HIPCHECK(hipMemsetAsync(abortFlag[0], 0, sizeof(uint32_t), stream[0]));
-  HIPCHECK(hipStreamSynchronize(stream[0]));
+  HIPCHECK(cudaSetDevice(device_id[0]));
+  HIPCHECK(cudaStreamCreateWithFlags(&stream[0], cudaStreamNonBlocking));
+  HIPCHECK(cudaDeviceEnablePeerAccess(device_id[1], 0));
+  HIPCHECK(cudaGetDeviceProperties(&prop[0], device_id[0]));
+  HIPCHECK(cudaMalloc((void**)&flag[0], HIP_IPC_MEM_MIN_SIZE));
+  HIPCHECK(cudaHostAlloc ((void**)&time_delta[0], sizeof(uint64_t), cudaHostAllocDefault));
+  HIPCHECK(cudaMalloc((void**)&abortFlag[0], sizeof(uint32_t)));
+  HIPCHECK(cudaMemsetAsync(flag[0], 0, HIP_IPC_MEM_MIN_SIZE, stream[0]));
+  HIPCHECK(cudaMemsetAsync(abortFlag[0], 0, sizeof(uint32_t), stream[0]));
+  HIPCHECK(cudaStreamSynchronize(stream[0]));
 
-  HIPCHECK(hipSetDevice(device_id[1]));
-  HIPCHECK(hipStreamCreateWithFlags(&stream[1], hipStreamNonBlocking));
-  HIPCHECK(hipDeviceEnablePeerAccess(device_id[0], 0));
-  HIPCHECK(hipGetDeviceProperties(&prop[1], device_id[1]));
-  HIPCHECK(hipExtMallocWithFlags((void**)&flag[1], HIP_IPC_MEM_MIN_SIZE, prop[1].gcnArch / 10 == 94 ? hipDeviceMallocUncached : hipDeviceMallocFinegrained));
-  HIPCHECK(hipHostMalloc((void**)&time_delta[1], sizeof(uint64_t), hipHostMallocDefault));
-  HIPCHECK(hipMalloc((void**)&abortFlag[1], sizeof(uint32_t)));
-  HIPCHECK(hipMemsetAsync(flag[1], 0, HIP_IPC_MEM_MIN_SIZE, stream[1]));
-  HIPCHECK(hipMemsetAsync(abortFlag[1], 0, sizeof(uint32_t), stream[0]));
-  HIPCHECK(hipStreamSynchronize(stream[1]));
+  HIPCHECK(cudaSetDevice(device_id[1]));
+  HIPCHECK(cudaStreamCreateWithFlags(&stream[1], cudaStreamNonBlocking));
+  HIPCHECK(cudaDeviceEnablePeerAccess(device_id[0], 0));
+  HIPCHECK(cudaGetDeviceProperties(&prop[1], device_id[1]));
+  HIPCHECK(cudaMalloc((void**)&flag[1], HIP_IPC_MEM_MIN_SIZE));
+  HIPCHECK(cudaHostAlloc((void**)&time_delta[1], sizeof(uint64_t), cudaHostAllocDefault));
+  HIPCHECK(cudaMalloc((void**)&abortFlag[1], sizeof(uint32_t)));
+  HIPCHECK(cudaMemsetAsync(flag[1], 0, HIP_IPC_MEM_MIN_SIZE, stream[1]));
+  HIPCHECK(cudaMemsetAsync(abortFlag[1], 0, sizeof(uint32_t), stream[0]));
+  HIPCHECK(cudaStreamSynchronize(stream[1]));
 
-  HIPCHECK(hipSetDevice(device_id[0]));
+  HIPCHECK(cudaSetDevice(device_id[0]));
   PingKernel<<<1, LL_MAX_THREADS, 0, stream[0]>>>(flag[0], flag[1], time_delta[0], abortFlag[0]);
 
-  HIPCHECK(hipSetDevice(device_id[1]));
+  HIPCHECK(cudaSetDevice(device_id[1]));
   PongKernel<<<1, LL_MAX_THREADS, 0, stream[1]>>>(flag[1], flag[0], time_delta[1], abortFlag[1]);
 
-  double vega_gpu_rtc_freq;
+  double gpu_rtc_freq;
 
-  HIPCHECK(hipStreamSynchronize(stream[0]));
-  vega_gpu_rtc_freq = (prop[0].gcnArch / 10 == 94) ? 1.0E8 : 2.5E7;
-  fprintf(stdout, "One-way latency in us: %g\n", double(*time_delta[0]) * 1e6 / NUM_LOOPS_RUN / vega_gpu_rtc_freq / 2);
+  HIPCHECK(cudaStreamSynchronize(stream[0]));
+  gpu_rtc_freq = prop[0].clockRate*1.0E3;
+  fprintf(stdout, "One-way latency in us: %g\n", double(*time_delta[0]) * 1e6 / NUM_LOOPS_RUN / gpu_rtc_freq / 2);
 
-  HIPCHECK(hipStreamSynchronize(stream[1]));
-  vega_gpu_rtc_freq = (prop[1].gcnArch / 10 == 94) ? 1.0E8 : 2.5E7;
-  fprintf(stdout, "One-way latency in us: %g\n", double(*time_delta[1]) * 1e6 / NUM_LOOPS_RUN / vega_gpu_rtc_freq / 2);
+  HIPCHECK(cudaStreamSynchronize(stream[1]));
+  gpu_rtc_freq = prop[1].clockRate*1.0E3;
+  fprintf(stdout, "One-way latency in us: %g\n", double(*time_delta[1]) * 1e6 / NUM_LOOPS_RUN / gpu_rtc_freq / 2);
 
-  HIPCHECK(hipFree(flag[0]));
-  HIPCHECK(hipFree(time_delta[0]));
-  HIPCHECK(hipFree(abortFlag[0]));
-  HIPCHECK(hipFree(flag[1]));
-  HIPCHECK(hipFree(time_delta[1]));
-  HIPCHECK(hipFree(abortFlag[1]));
+  HIPCHECK(cudaFree(flag[0]));
+  HIPCHECK(cudaFreeHost(time_delta[0]));
+  HIPCHECK(cudaFree(abortFlag[0]));
+  HIPCHECK(cudaFree(flag[1]));
+  HIPCHECK(cudaFreeHost(time_delta[1]));
+  HIPCHECK(cudaFree(abortFlag[1]));
   return 0;
 }
