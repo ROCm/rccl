2b1c7a8fe5549fc72d9d88f8c14e5644bbea9476
Wenkai Du <wenkai.du@amd.com>
2024-06-03 11:22:21 -0500

    Use non-temporal store for access less than 16 bytes

--- src/device/op128.h
+++ src/device/op128.h
@@ -200,7 +200,7 @@ template<> __device__ __forceinline__ void st_global<0>(uintptr_t addr, BytePack
   template<> \
   __device__ __forceinline__ void st_##space<bytes>(addr_cxx_ty addr, BytePack<bytes> value) { \
     data_cxx_ty tmp = value.native; \
-    *((data_cxx_ty *)addr) = tmp; \
+    __builtin_nontemporal_store(tmp, (data_cxx_ty *)addr); \
   }
 
 // #if __CUDA_ARCH__ >= 700
