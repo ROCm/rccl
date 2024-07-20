b40e701ef0261d01f890b28f493b5a2ed1dc9dab
nileshnegi <Nilesh.Negi@amd.com>
2024-07-20 18:38:47 -0500

    [BUILD] Build MSCCLPP only for gfx942
    Signed-off-by: nileshnegi <Nilesh.Negi@amd.com>

--- cmake/MSCCLPP.cmake
+++ cmake/MSCCLPP.cmake
@@ -41,14 +41,14 @@ if(ENABLE_MSCCLPP)
     find_package(mscclpp_nccl)
 
     if(NOT mscclpp_nccl_FOUND)
-        message(STATUS "MSCCL++ not found. Downloading and building MSCCL++.")
+        message(STATUS "MSCCL++ not found. Downloading and building MSCCL++ only for gfx942.")
         # Download, build and install mscclpp
     
         download_project(PROJ                mscclpp_nccl
                          GIT_REPOSITORY      https://github.com/microsoft/mscclpp.git
                          GIT_TAG             b1b9d0626cfa40319c18c05f8c16650568395c29
                          INSTALL_DIR         ${MSCCLPP_ROOT}
-                         CMAKE_ARGS          -DGPU_TARGETS=${GPU_TARGETS} -DBYPASS_GPU_CHECK=ON -DUSE_ROCM=ON -DCMAKE_BUILD_TYPE=Release -DBUILD_APPS_NCCL=ON -DBUILD_PYTHON_BINDINGS=OFF -DBUILD_TESTS=OFF -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
+                         CMAKE_ARGS          -DGPU_TARGETS=gfx942 -DBYPASS_GPU_CHECK=ON -DUSE_ROCM=ON -DCMAKE_BUILD_TYPE=Release -DBUILD_APPS_NCCL=ON -DBUILD_PYTHON_BINDINGS=OFF -DBUILD_TESTS=OFF -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
                          LOG_DOWNLOAD        FALSE
                          LOG_CONFIGURE       FALSE
                          LOG_BUILD           FALSE
