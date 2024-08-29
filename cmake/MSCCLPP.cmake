60411c2606f5b731a677b3409ab8b82bd1f831d3
Nilesh M Negi <Nilesh.Negi@amd.com>
2024-08-29 17:58:11 -0400

    [BUILD] Support custom CMake flags in MSCCLPP (#1275)
    * [BUILD] Support custom CMAKE_PREFIX_PATH in MSCCLPP
    
    Signed-off-by: nileshnegi <Nilesh.Negi@amd.com>
    
    * [BUILD] CMake flags to support build-id in MSCCLPP
    
    Signed-off-by: nileshnegi <Nilesh.Negi@amd.com>
    
    * [BUILD] Fix CMake warnings in MSCCLPP build
    
    Signed-off-by: nileshnegi <Nilesh.Negi@amd.com>
    
    * Wrapped all cmake arguments passed to mscclpp to remove empty arguments and properly format them.
    
    ---------
    
    Signed-off-by: nileshnegi <Nilesh.Negi@amd.com>
    Co-authored-by: Corey Derochie <corey.derochie@amd.com>

--- cmake/MSCCLPP.cmake
+++ cmake/MSCCLPP.cmake
@@ -32,6 +32,18 @@
 # For downloading, building, and installing required dependencies
 include(cmake/DownloadProject.cmake)
 
+function(mscclpp_cmake_arg NAME)
+    string (REPLACE ";" "$<SEMICOLON>" ARG_VALUE "${${NAME}}") # Replace ; with non-escapable SEMICOLON symbol to avoid CMake errors
+    string(STRIP "${ARG_VALUE}" ARG_VALUE) # Eliminate whitespace, reducing to empty string if necessary
+
+    # Only add a cmake argument if it has a value
+    set(${NAME}_ARG "-D${NAME}=\"${ARG_VALUE}\"" PARENT_SCOPE)
+    if("${ARG_VALUE}" STREQUAL "")
+        set(${NAME}_ARG "" PARENT_SCOPE)
+    endif()
+endfunction()
+
+
 if(ENABLE_MSCCLPP)
     set(MSCCLPP_ROOT ${CMAKE_CURRENT_SOURCE_DIR}/ext/mscclpp CACHE PATH "")
     execute_process(
@@ -43,12 +55,18 @@ if(ENABLE_MSCCLPP)
     if(NOT mscclpp_nccl_FOUND)
         message(STATUS "MSCCL++ not found. Downloading and building MSCCL++ only for gfx942.")
         # Download, build and install mscclpp
+        
+        mscclpp_cmake_arg(CMAKE_PREFIX_PATH)
+        mscclpp_cmake_arg(CMAKE_SHARED_LINKER_FLAGS_INIT)
+        mscclpp_cmake_arg(CMAKE_EXE_LINKER_FLAGS_INIT)
+        mscclpp_cmake_arg(CMAKE_INSTALL_RPATH_USE_LINK_PATH)
+        mscclpp_cmake_arg(HIP_COMPILER)
     
         download_project(PROJ                mscclpp_nccl
                          GIT_REPOSITORY      https://github.com/microsoft/mscclpp.git
                          GIT_TAG             8c6fb429e92e07acb82c0fdcdab44854fc63aa68
                          INSTALL_DIR         ${MSCCLPP_ROOT}
-                         CMAKE_ARGS          -DGPU_TARGETS=gfx942 -DBYPASS_GPU_CHECK=ON -DUSE_ROCM=ON -DCMAKE_BUILD_TYPE=Release -DBUILD_APPS_NCCL=ON -DBUILD_PYTHON_BINDINGS=OFF -DBUILD_TESTS=OFF -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
+                         CMAKE_ARGS          -DGPU_TARGETS=gfx942 -DBYPASS_GPU_CHECK=ON -DUSE_ROCM=ON -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DBUILD_APPS_NCCL=ON -DBUILD_PYTHON_BINDINGS=OFF -DBUILD_TESTS=OFF -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR> "${CMAKE_PREFIX_PATH_ARG}" "${CMAKE_SHARED_LINKER_FLAGS_INIT_ARG}" "${CMAKE_EXE_LINKER_FLAGS_INIT_ARG}" -DCMAKE_VERBOSE_MAKEFILE=1 "${CMAKE_INSTALL_RPATH_USE_LINK_PATH_ARG}" "${HIP_COMPILER_ARG}"
                          LOG_DOWNLOAD        FALSE
                          LOG_CONFIGURE       FALSE
                          LOG_BUILD           FALSE
@@ -65,6 +83,6 @@ if(ENABLE_MSCCLPP)
     set(MSCCLPP_OUT_LIBS "")
     foreach(LIB_NAME ${MSCCLPP_LIB_NAMES})
         list(APPEND MSCCLPP_OUT_LIBS ${PROJECT_BINARY_DIR}/${LIB_NAME})
-    endforeach()  
+    endforeach()
     file(COPY ${MSCCLPP_LIB_FILES} DESTINATION ${PROJECT_BINARY_DIR})
 endif()
