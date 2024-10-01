f3669149c18df0b87b7fc61fcc5098db2018f71c
PedramAlizadeh <pmohamma@amd.com>
2024-10-01 15:19:53 -0500

    modifying PATTERN_TREE case

--- src/graph/rome_models.cc
+++ src/graph/rome_models.cc
@@ -2092,7 +2092,7 @@ ncclResult_t parseRome4P2H(struct ncclTopoSystem* system, struct ncclTopoGraph* 
       }
     }
     break;
-  case NCCL_TOPO_PATTERN_BALANCED_TREE:
+  case NCCL_TOPO_PATTERN_BALANCED_TREE: case NCCL_TOPO_PATTERN_TREE:
     if (romeTopoModels[i].treeBase != nullptr) {
       NCCLCHECK(parseGraphLight(romeTopoModels[i].treeBase, system, graph, g));
       if (graph->nChannels) return ncclSuccess;
@@ -2116,12 +2116,6 @@ ncclResult_t parseRome4P2H(struct ncclTopoSystem* system, struct ncclTopoGraph* 
           }
         }
       }
-    }
-    break;
-  case NCCL_TOPO_PATTERN_TREE:
-    if (romeTopoModels[i].treeBase != nullptr) {
-      NCCLCHECK(parseGraphLight(romeTopoModels[i].treeBase, system, graph, g));
-      if (graph->nChannels) return ncclSuccess;
     }
     break;
   }
