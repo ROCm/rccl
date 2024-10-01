efd72553bc503df47b69040751902f11dd3b71b4
Nusrat Islam <Nusrat.Islam@amd.com>
2024-10-01 08:43:51 -0500

    Enable MSCCLPP use in CPX mode
    This PR enables the use of MSCCLPP in CPX mode for 8 GPUs.

--- cmake/MSCCLPP.cmake
+++ cmake/MSCCLPP.cmake
@@ -63,7 +63,10 @@ if(ENABLE_MSCCLPP)
                 WORKING_DIRECTORY ${MSCCLPP_SOURCE}
             )
         endif()
-        
+        execute_process(
+           COMMAND git apply ${CMAKE_CURRENT_SOURCE_DIR}/ext-src/cpx.patch
+           WORKING_DIRECTORY ${MSCCLPP_SOURCE}
+        ) 
         message(STATUS "Building mscclpp only for gfx942.")
         
         mscclpp_cmake_arg(CMAKE_PREFIX_PATH)
