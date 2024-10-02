39d072f38703fa720ce30830792d0462a92cad07
akolliasAMD <akollias@amd.com>
2024-10-02 19:42:12 -0400

    .jenkins renabled all tests

--- .jenkins/common.groovy
+++ .jenkins/common.groovy
@@ -17,9 +17,6 @@ def runCompileCommand(platform, project, jobName)
 
 def runTestCommand (platform, project, gfilter, envars)
 {
-    // Temporarily disable Standalone.RegressionTiming for RCCL CI
-    gfilter = "*:-Standalone.Regression*"
-
     String sudo = auxiliary.sudo(platform.jenkinsLabel)
 
     def command = """#!/usr/bin/env bash
@@ -28,7 +25,7 @@ def runTestCommand (platform, project, gfilter, envars)
                 cd ${project.paths.project_build_prefix}/build/release/test
                 ${sudo} ulimit -l unlimited
                 ulimit -a
-                ${sudo} ${envars} LD_LIBRARY_PATH=\${RUN_TEST_ROOT}/${project.paths.project_build_prefix}/build/release:\${LD_LIBRARY_PATH} RCCL_ENABLE_SIGNALHANDLER=1 NCCL_DEBUG=INFO HSA_FORCE_FINE_GRAIN_PCIE=1 UT_MULTITHREAD=1 UT_PROCESS_MASK=1 ./rccl-UnitTests --gtest_filter=${gfilter} --gtest_output=xml --gtest_color=yes
+                ${sudo} ${envars} LD_LIBRARY_PATH=\${RUN_TEST_ROOT}/${project.paths.project_build_prefix}/build/release:\${LD_LIBRARY_PATH} RCCL_ENABLE_SIGNALHANDLER=1 NCCL_DEBUG=INFO HSA_FORCE_FINE_GRAIN_PCIE=1 UT_MULTITHREAD=1 UT_PROCESS_MASK=1 ./rccl-UnitTests --gtest_output=xml --gtest_color=yes
             """
 
    platform.runCommand(this, command)
