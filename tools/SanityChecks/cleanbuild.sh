4adb7b92316a1f58d575fbd2acfb27599479e9ef
Chao Chen <cchen104@amd.com>
2024-10-30 04:51:52 +0800

    refactoring scripts

--- tools/SanityChecks/cleanbuild.sh
+++ tools/SanityChecks/cleanbuild.sh
@@ -4,23 +4,8 @@
 cd ../../ || exit 1
 
 # Clean up any previous builds and create a fresh build directory
-rm -rf build
-mkdir build
-cd build || exit 1
+rm -rf build || exit 1
 
-# Specify the path to the CMake binary
-CMAKE_BIN_PATH="../cmake-3.28.2/bin/cmake"
-
-# Run CMake with the desired configuration
-if ! "$CMAKE_BIN_PATH" -DCMAKE_PREFIX_PATH=/home/apotnuru/bugs/rccl -DCMAKE_BUILD_TYPE=Debug ..; then
-    echo "CMake configuration failed!"
-    exit 1
-fi
-
-# Build the project with all available CPU cores
-if ! make -j"$(nproc)"; then
-    echo "Build failed!"
-    exit 1
-fi
-
+#uses install.sh script for building rccl library
+./install.sh --debug --disable-mscclpp --local_gpu_only || exit 1
 echo "Build completed successfully!"
--- tools/SanityChecks/run.sh
+++ tools/SanityChecks/run.sh
@@ -1,3 +1,8 @@
-LIB_PATH_DIR=/home/apotnuru/bugs/rccl/build/release
+make clean
+make || exit 1
+
+echo -e "\n\n End of compiling the test program, starting memory leak check \n\n"
+
+LIB_PATH_DIR=$PWD/../../build/debug
 echo $LIB_PATH_DIR
-LD_LIBRARY_PATH=$LIB_PATH_DIR valgrind --leak-check=full ./MemleakTest 0 1
+LD_LIBRARY_PATH=$LIB_PATH_DIR NCCL_DEBUG=INFO valgrind --leak-check=full ./MemleakTest 0 1
