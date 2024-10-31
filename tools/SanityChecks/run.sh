0224efd666a1365dd2b45180389e368e48b69851
Chao Chen <cchen104@amd.com>
2024-11-01 01:01:51 +0800

    Updated to sanity checks folder

--- tools/SanityChecks/run.sh
+++ tools/SanityChecks/run.sh
@@ -5,4 +5,4 @@ echo -e "\n\n End of compiling the test program, starting memory leak check \n\n
 
 LIB_PATH_DIR=$PWD/../../build/debug
 echo $LIB_PATH_DIR
-LD_LIBRARY_PATH=$LIB_PATH_DIR NCCL_DEBUG=INFO valgrind --leak-check=full ./MemleakTest 0 1
+sudo LD_LIBRARY_PATH=$LIB_PATH_DIR NCCL_DEBUG=INFO valgrind --leak-check=full ./MemleakTest 0 4
