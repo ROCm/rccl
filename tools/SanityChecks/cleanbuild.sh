faa18401c7bb457f749b433cda35c7b99db08b8d
PJAvinash <avinashindian2.0@gmail.com>
2024-11-12 17:33:23 +0000

    changes in tools

--- tools/SanityChecks/cleanbuild.sh
+++ tools/SanityChecks/cleanbuild.sh
@@ -1,11 +1,32 @@
-#!/bin/bash
+# #!/bin/bash
 
-# Navigate to the project root
-cd ../../ || exit 1
+# # Navigate to the project root
+# cd ../../ || exit 1
 
-# Clean up any previous builds and create a fresh build directory
-rm -rf build || exit 1
+# # Clean up any previous builds and create a fresh build directory
+# rm -rf build || exit 1
 
 #uses install.sh script for building rccl library
-./install.sh --debug --disable-mscclpp --local_gpu_only || exit 1
-echo "Build completed successfully!"
+#./install.sh --debug --disable-mscclpp --local_gpu_only || exit 1
+
+#echo "Build completed successfully!"
+cd ../../
+rm -rf build
+mkdir build
+cd build
+
+# Specify the correct path to the CMake binary
+CMAKE_BIN_PATH=cmake #../cmake-3.28.2/bin/cmake
+# -DCMAKE_BUILD_TYPE=Debug
+
+if ! $CMAKE_BIN_PATH ..  -DCMAKE_PREFIX_PATH=/opt/rocm; then
+    echo "CMake configuration failed!"
+    exit 1
+fi
+
+if ! make -j$(nproc); then
+    echo "Build failed!"
+    exit 1
+fi
+
+echo "Build completed"
