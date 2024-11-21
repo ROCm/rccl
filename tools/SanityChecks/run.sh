fae9c898f20a8b30feb569e9af234be3c2b429f7
PJAvinash <avinashindian2.0@gmail.com>
2024-11-21 21:26:58 +0000

    pointing RCCL lib build to debug version

--- tools/SanityChecks/run.sh
+++ tools/SanityChecks/run.sh
@@ -3,6 +3,6 @@ make || exit 1
 
 echo -e "\n\n End of compiling the test program, starting memory leak check \n\n"
 
-LIB_PATH_DIR=$PWD/../../build/
+LIB_PATH_DIR=$PWD/../../build/debug/
 echo $LIB_PATH_DIR
 LD_LIBRARY_PATH=$LIB_PATH_DIR NCCL_DEBUG=INFO  valgrind --leak-check=full --show-leak-kinds=all ./MemleakTest 0 1
