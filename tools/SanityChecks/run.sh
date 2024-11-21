48d592821784fb7adad43b5710e1326edfcf6c18
PJAvinash <avinashindian2.0@gmail.com>
2024-11-21 23:45:11 +0000

    Removing sanity checks

--- tools/SanityChecks/run.sh
+++
@@ -1,8 +0,0 @@
-make clean
-make || exit 1
-
-echo -e "\n\n End of compiling the test program, starting memory leak check \n\n"
-
-LIB_PATH_DIR=$PWD/../../build/debug/
-echo $LIB_PATH_DIR
-LD_LIBRARY_PATH=$LIB_PATH_DIR NCCL_DEBUG=INFO  valgrind --leak-check=full --show-leak-kinds=all ./MemleakTest 0 1
