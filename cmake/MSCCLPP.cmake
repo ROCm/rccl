6b5e8166234164fbedfd2d3c84a5b668cd45599a
isaki001 <Ioannis.Sakiotis@amd.com>
2025-05-06 09:29:40 -0500

    re-apply remove-clip.patch due to device-flag.patch update

--- cmake/MSCCLPP.cmake
+++ cmake/MSCCLPP.cmake
@@ -98,6 +98,10 @@ if(ENABLE_MSCCLPP)
 	    WORKING_DIRECTORY ${MSCCLPP_SOURCE}
 	)
 
+	execute_process(
+	    COMMAND git apply ${CMAKE_CURRENT_SOURCE_DIR}/ext-src/remove-clip.patch
+	    WORKING_DIRECTORY ${MSCCLPP_SOURCE}
+	)
         set(CMAKE_INHERITED_ARGS "")
         set(CMAKE_ARGS_LIST "CMAKE_PREFIX_PATH;CMAKE_INSTALL_RPATH_USE_LINK_PATH;HIP_COMPILER")
         foreach(arg IN LISTS CMAKE_ARGS_LIST)
@@ -140,6 +144,12 @@ if(ENABLE_MSCCLPP)
         )
 
         find_package(mscclpp_nccl REQUIRED)
+
+	execute_process(
+	    COMMAND git apply --reverse ${CMAKE_CURRENT_SOURCE_DIR}/ext-src/remove-clip.patch
+	    WORKING_DIRECTORY ${MSCCLPP_SOURCE}
+	)
+
 
 	execute_process(
 	    COMMAND git apply --reverse ${CMAKE_CURRENT_SOURCE_DIR}/ext-src/device-flag.patch
