8a3808231a1537afecc29ce7b7105cb84254beec
Dingming Wu <dmwu0506@gmail.com>
2025-05-12 08:35:43 -0700

    avoid unnecessary info msg on hsa_no_scratch_reclaim detection

--- src/init.cc
+++ src/init.cc
@@ -186,9 +186,7 @@ static ncclResult_t ncclInit() {
   int hipRuntimeVersion = 0;
   // hipVer is an integer e.g., 6.2.41133 -> 60241133
   CUDACHECK(hipRuntimeGetVersion(&hipRuntimeVersion));
-  if ((hsaScratchEnv && strcmp(hsaScratchEnv,"1") == 0) || hipRuntimeVersion >= 60400000){
-    INFO(NCCL_INIT, "HSA_NO_SCRATCH_RECLAIM : %s, hipVer:%d", hsaScratchEnv, hipRuntimeVersion);
-  }else{
+  if ((!hsaScratchEnv || strcmp(hsaScratchEnv,"1") != 0) && hipRuntimeVersion < 60400000){
     WARN("HSA_NO_SCRATCH_RECLAIM=1 must be set to avoid RCCL perf hit for rocm older than 6.4,, rocm ver:%d", hipRuntimeVersion);
   }
   pthread_once(&initOnceControl, initOnceFunc);
