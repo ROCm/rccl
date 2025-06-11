a68adc27e48d01d4d0802af160e4b6c6c1f4e770
amd-jmacaran <joseph.macaranas@amd.com>
2025-06-11 14:20:18 -0400

    new attempts

--- .azuredevops/slurm/build.sh
+++ .azuredevops/slurm/build.sh
@@ -3,8 +3,12 @@
 #SBATCH --output=rccl-build-%j.out
 #SBATCH --error=rccl-build-%j.err
 #SBATCH --time=60
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
 #SBATCH --partition=gt
 
+source /etc/profile.d/lmod.sh
+module load rocm/6.4.0
 module list
 cd "${SLURM_SUBMIT_DIR:-$PWD}"
 mkdir -p build
--- .azuredevops/slurm/test.sh
+++ .azuredevops/slurm/test.sh
@@ -5,6 +5,8 @@
 #SBATCH --time=60
 #SBATCH --partition=gt
 
+source /etc/profile.d/lmod.sh
+module load rocm/6.4.0
 module list
 cd "${SLURM_SUBMIT_DIR:-$PWD}/build/test"
 ./rccl-UnitTests --gtest_output=xml:./test_output.xml --gtest_color=yes
