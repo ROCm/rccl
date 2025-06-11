1fc2db23a7ae1f4c8ceaed0ba9b3fe9b033c6aeb
Arm Patinyasakdikul <arm.patinyasakdikul@amd.com>
2025-06-11 09:12:57 -0500

    Added missing copyright message.

--- src/register/sendrecv_reg.cc
+++ src/register/sendrecv_reg.cc
@@ -1,3 +1,5 @@
+/* Copyright © Advanced Micro Devices, Inc., or its affiliates. */
+
 #include "register.h"
 #include "transport.h"
 
--- tools/RcclReplayer/rcclReplayer.hpp
+++ tools/RcclReplayer/rcclReplayer.hpp
@@ -1,3 +1,5 @@
+/* Copyright © Advanced Micro Devices, Inc., or its affiliates. */
+
 #pragma once
 #include <map>
 #include <cstring>
