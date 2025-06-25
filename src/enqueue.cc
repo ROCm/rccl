9e1eff6bb574be68127545c495322af0f3cdea15
Arm Patinyasakdikul <arm.patinyasakdikul@amd.com>
2025-06-25 15:30:46 -0500

    add warning if workFIFO is not available after multiple retries.

--- src/enqueue.cc
+++ src/enqueue.cc
@@ -1187,6 +1187,8 @@ static ncclResult_t scheduleP2pTasksToPlan(
 // Spin until its safe to increase comm->workFifoProduced to desiredProduced.
 static void waitWorkFifoAvailable(struct ncclComm* comm, uint32_t desiredProduced) {
   bool hasRoom = (desiredProduced - comm->workFifoConsumedLeast) <= comm->workFifoBytes;
+  uint64_t count = 0;
+  int warned = 0;
   if (hasRoom) return;
   while (true) {
     // We have to poll for notifications from device.
@@ -1225,6 +1227,17 @@ static void waitWorkFifoAvailable(struct ncclComm* comm, uint32_t desiredProduce
     hasRoom = (desiredProduced - comm->workFifoConsumedLeast) <= comm->workFifoBytes;
     if (hasRoom) break;
     sched_yield();
+
+    /* Warn if we get stuck waiting for workFifo. */
+    count++;
+    if (warned == 0 && count == 100000 && comm->rank == 0) {
+      warned = 1;
+      WARN("Waiting for work FIFO to become available."
+           "Work fifo exhaustion can happen in large scale/high iteration count of alltoall."
+           "In order to increase work FIFO size, set NCCL_WORK_FIFO_BYTES to higher number"
+           " (current: %ld). \n\nRCCL continues to retry...", comm->workFifoBytes);
+    }
+
   }
 }
 
