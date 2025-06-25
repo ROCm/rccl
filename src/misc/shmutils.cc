89ced7a0924bb057e739d07a55fe9d50778da8cb
Wenkai Du <wenkai.du@amd.com>
2025-06-25 15:58:52 -0500

    Use hipHostRegisterUncached

--- src/misc/shmutils.cc
+++ src/misc/shmutils.cc
@@ -115,7 +115,7 @@ ncclResult_t ncclShmOpen(char* shmPath, size_t shmPathSize, size_t shmSize, void
 
   if (devShmPtr) {
 #if defined(HIP_HOST_UNCACHED_MEMORY)
-    CUDACHECKGOTO(cudaHostRegister((void*)hptr, realShmSize, cudaHostRegisterPortable | cudaHostRegisterMapped | hipHostMallocUncached), ret, fail);
+    CUDACHECKGOTO(cudaHostRegister((void*)hptr, realShmSize, cudaHostRegisterPortable | cudaHostRegisterMapped | hipHostRegisterUncached), ret, fail);
 #else
     CUDACHECKGOTO(cudaHostRegister((void*)hptr, realShmSize, cudaHostRegisterPortable | cudaHostRegisterMapped), ret, fail);
 #endif
