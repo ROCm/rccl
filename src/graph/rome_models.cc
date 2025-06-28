d73e3763bfb99d1e687c9bba0a4b01498e697f10
isaki001 <ioannissakiotis@gmail.com>
2025-06-27 20:53:45 -0400

    remove erroneous string

--- src/graph/rome_models.cc
+++ src/graph/rome_models.cc
@@ -1953,7 +1953,7 @@ ncclResult_t parseA2a8P(struct ncclTopoSystem* system, struct ncclTopoGraph* gra
   INFO(NCCL_GRAPH, "%s", line);
   system->type |= RCCL_TOPO_4P2H_ROME;
   parseOptions(system, romeTopoModels[i].options);
-  if(IsArchMatch(system->nodes[GPU].nodes[0].gpu.gcn, "gfx950")){Add commentMore actions
+  if(IsArchMatch(system->nodes[GPU].nodes[0].gpu.gcn, "gfx950")){
     system->tuning = 6;
   }
   // create 4P2H based on reference and remapped ids
@@ -2154,7 +2154,7 @@ ncclResult_t parseRome4P2H(struct ncclTopoSystem* system, struct ncclTopoGraph* 
   }
   INFO(NCCL_GRAPH, "%s", line);
   parseOptions(system, romeTopoModels[i].options);
-  if(IsArchMatch(system->nodes[GPU].nodes[0].gpu.gcn, "gfx950")){Add commentMore actions
+  if(IsArchMatch(system->nodes[GPU].nodes[0].gpu.gcn, "gfx950")){
     system->tuning = 6;
   }
   // create 4P2H based on reference and remapped ids
@@ -2343,7 +2343,7 @@ ncclResult_t parse1H16P(struct ncclTopoSystem* system, struct ncclTopoGraph* gra
   INFO(NCCL_GRAPH, "%s", line);
   system->type |= RCCL_TOPO_16P1H;
   parseOptions(system, romeTopoModels[i].options);
-  if(IsArchMatch(system->nodes[GPU].nodes[0].gpu.gcn, "gfx950")){Add commentMore actions
+  if(IsArchMatch(system->nodes[GPU].nodes[0].gpu.gcn, "gfx950")){
     system->tuning = 6;
   }
 
