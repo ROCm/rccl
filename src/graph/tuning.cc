33e5fa4584bdd979e1307c34c10ab4d24070b0a6
Mustafa Abduljabbar <mustafa.abduljabbar@amd.com>
2025-07-02 10:24:07 -0700

    Adjust tuning factor impacting more than 2 nodes

--- src/graph/tuning.cc
+++ src/graph/tuning.cc
@@ -334,7 +334,7 @@ static struct tuningModel tuning_model_5 {
     /*AllGather*/
     {/*LL (min/max/factor/thread_threshold)*/ {0, 98304,  1, 16}, /*LL64/128 (min/max/factor/thread_threshold)*/ {98304, 5046272, 1, 64}},
     /*AllReduce*/
-    {/*LL (min/max/factor/thread_threshold)*/ {0, 1048576, 1, 0},/*LL64/128 (min/max/factor/thread_threshold)*/ {1048576, 9437184, 3145728, 0}},
+    {/*LL (min/max/factor/thread_threshold)*/ {0, 1048576, 1, 0},/*LL64/128 (min/max/factor/thread_threshold)*/ {1048576, 9437184, 81205728, 0}},
   },
 };
 
