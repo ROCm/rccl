fffec0a48c7b910365fc0408beb02304064938ef
Mustafa Abduljabbar <mustafa.abduljabbar@amd.com>
2025-07-11 07:49:41 -0700

    Remove extra function decl

--- src/include/rccl_common.h
+++ src/include/rccl_common.h
@@ -74,7 +74,6 @@ inline size_t rcclGetSizePerRank(ncclFunc_t const& func, size_t const& nBytes, i
 ncclResult_t rcclGetAlgoProtoIndex(const char *envStr, const char* algoProtoString[], int nEntries, int& result);
 ncclResult_t rcclOverrideAlgoOrProto(const char* ncclAlgoStr[], const char* ncclProtoStr[], float table[][NCCL_NUM_PROTOCOLS],  struct ncclTaskColl* info);
 void rcclUpdateCollectiveProtocol(struct ncclComm* comm, size_t const& nBytes, struct ncclTaskColl* info);
-void rcclGetAlgoProto(const char *envStr, const char* algoProtoString[], int nEntries, int& result);
 void rcclUpdateThreadThreshold(struct ncclComm* comm, size_t const& nBytes, struct ncclTaskColl* info, int& threadThreshold);
 ncclResult_t rcclGetAlgoInfo(struct ncclComm* comm, ncclFunc_t coll, uint64_t count, ncclDataType_t dataType,
                              int collNetSupport, int nvlsSupport, int numPipeOps,
