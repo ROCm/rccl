f7a971680da90d3f7c3258cdb6e118a0ba35f408
Mustafa Abduljabbar <muabdulj@amd.com>
2025-07-28 20:57:31 -0500

    Support gfx950 in topo_expl

--- tools/topo_expl/include/device_table.h
+++ tools/topo_expl/include/device_table.h
@@ -11,5 +11,8 @@ static struct rcclKernelItem rcclKernelTable[] = { };
 
 template <int unroll>
 __forceinline__ __device__ void NCCL_CALL_FUNCTIONS(unsigned short funcIndex) noexcept { }
+__forceinline__ __device__ void NCCL_CALL_FUNCTIONS_1(unsigned short funcIndex) noexcept { }
+__forceinline__ __device__ void NCCL_CALL_FUNCTIONS_2(unsigned short funcIndex) noexcept { }
+__forceinline__ __device__ void NCCL_CALL_FUNCTIONS_4(unsigned short funcIndex) noexcept { }
 
 #endif
