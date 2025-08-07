06878c2eede57013ef6078033177fd6210090964
isaki001 <ioannissakiotis@gmail.com>
2025-08-07 14:28:20 +0000

    set multinode allreduce to start simple on 64MB for gfx950

--- src/graph/tuning.cc
+++ src/graph/tuning.cc
@@ -373,7 +373,7 @@ static struct tuningModel tuning_model_6 {
     /*AllGather*/
     {/*LL (min/max/factor/thread_threshold)*/ {0, 7,  1, 16}, /*LL64/128 (min/max/factor/thread_threshold)*/ {7, 8388608, 1, 64}},
     /*AllReduce*/
-    {/*LL (min/max/factor/thread_threshold)*/ {0, 131071, 1, 0},/*LL64/128 (min/max/factor/thread_threshold)*/ {131071, 35320455, 3145728, 0}},
+    {/*LL (min/max/factor/thread_threshold)*/ {0, 131071, 1, 0},/*LL64/128 (min/max/factor/thread_threshold)*/ {131071, 17660227, 3145728, 0}},
     /*Reduce*/
     {/*LL (min/max/factor/thread_threshold)*/ {0, 16383, 1, 0},/*LL64/128 (min/max/factor/thread_threshold)*/ {16383, 16777216, 1, 0}},
     /*Broadcast*/
