d79ab210b920abee232b0be5b24666ce0b4c2dc4
ishkool <ishkool2@amd.com>
2025-08-12 13:47:36 +0530

    Update CommTest.cpp
    Removing info and tesbed as not required.

--- test/CommTest.cpp
+++ test/CommTest.cpp
@@ -3,8 +3,8 @@
  *
  * See LICENSE.txt for license information
  ************************************************************************/
+#include "gtest/gtest.h"
 #include "comm.h"
-#include "TestBed.hpp"
 
 
 
@@ -12,18 +12,12 @@ namespace RcclUnitTesting
 {
   TEST(CommTest, Sorter)
   {
-	TestBed testBed;
-	INFO("[CommTest] starting the test with %d GPUs\n", testBed.numDevicesAvailable);
 	// Configuration
 	ncclTaskCollSorter* me_ptr = new ncclTaskCollSorter;
 	me_ptr->head = nullptr;
 
 	ASSERT_EQ(ncclTaskCollSorterEmpty(me_ptr), true);
 	delete me_ptr;
-
-	INFO("[CommTest] Completed the test\n");
-	testBed.Finalize();
-
   }
 }
 
