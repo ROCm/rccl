52ae99a2e6bace94fa60fefceddbf20858d3d811
Mustafa Abduljabbar <muabdulj@amd.com>
2025-08-13 10:20:59 -0500

    Add clear error message

--- src/include/device.h
+++ src/include/device.h
@@ -730,7 +730,7 @@ inline int ncclDevFuncId(int coll, int devRedOp, int type, int algo, int proto) 
     row = it->second;
   }
   if(row < 0) {
-    WARN("Fatal error: ncclDevFuncId: %llu not found", key);
+    WARN("Fatal error: ncclDevFuncId: %llu not found for coll: %d, algo: %d, proto: %d, devRedOp: %d, type: %d", key, coll, algo, proto, devRedOp, type);
     return -1;
   }
   return row;
