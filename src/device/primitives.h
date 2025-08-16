ea7615ee9782931d23ce7aa6234a48af19b122ad
Mustafa Abduljabbar <mustafa.abduljabbar@amd.com>
2025-08-16 12:09:47 -0700

    add useAcc to template

--- src/device/primitives.h
+++ src/device/primitives.h
@@ -137,7 +137,7 @@ struct FanSymmetric {
 };
 
 // The primitives class. Specialized per protocol in the other headers.
-template<typename T, typename RedOp, typename Fan, int Direct, typename Proto, int P2p, bool isNetOffload = false, int Metadata = RCCL_METADATA_EMPTY, int Pipeline = 0>
+template<typename T, typename RedOp, typename Fan, int Direct, typename Proto, int P2p, bool isNetOffload = false, int Metadata = RCCL_METADATA_EMPTY, int Pipeline = 0, int useAcc = 0>
 class Primitives;
 
 // Used by LL & LL128 to implement direct members in the naive way.
