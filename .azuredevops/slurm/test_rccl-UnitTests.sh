826be4c8283c001ee9d5a216435adbf2a7b9cb65
Nilesh M Negi <Nilesh.Negi@amd.com>
2025-08-27 16:59:17 -0500

    Remove gtest_filter limitation from UT

--- .azuredevops/slurm/test_rccl-UnitTests.sh
+++ .azuredevops/slurm/test_rccl-UnitTests.sh
@@ -13,4 +13,4 @@ echo "Node identifier: $short_id"
 source /etc/profile.d/lmod.sh
 module load rocm/6.4.1
 cd "$BINARIES_DIR/bin"
-LD_LIBRARY_PATH="$BINARIES_DIR/lib:$LD_LIBRARY_PATH" NCCL_DEBUG=INFO RCCL_ENABLE_SIGNALHANDLER=1 HSA_NO_SCRATCH_RECLAIM=1 ./rccl-UnitTests --gtest_filter="StandaloneTests.*" --gtest_output=xml:$PIPELINE_WORKSPACE/rccl-UnitTests_output.xml --gtest_color=yes
+LD_LIBRARY_PATH="$BINARIES_DIR/lib:$LD_LIBRARY_PATH" NCCL_DEBUG=INFO RCCL_ENABLE_SIGNALHANDLER=1 HSA_NO_SCRATCH_RECLAIM=1 ./rccl-UnitTests --gtest_output=xml:$PIPELINE_WORKSPACE/rccl-UnitTests_output.xml --gtest_color=yes
