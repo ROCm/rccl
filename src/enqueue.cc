d4d6268ab052f420071b6ffe190f22964fb57a08
Alex Breslow <Alex.Breslow@amd.com>
2025-09-05 16:54:46 -0500

    Hacky fix, but I confirmed that it works.

--- src/enqueue.cc
+++ src/enqueue.cc
@@ -2154,8 +2154,8 @@ rccl_static ncclResult_t getAlgoInfo(
       regBuff = (regSendBuf && regRecvBuf && isSendValid && isRecvValid) || (ncclCudaGraphValid(comm->planner.capturingGraph) && ncclParamGraphRegister());
       if (regBuff && (info->func == ncclFuncAllGather || info->func == ncclFuncReduceScatter)) {
         if ((comm->nNodes > 1 && collNetSupport && nvlsSupport) || (comm->nNodes == 1 && nvlsSupport)) {
-          int recChannels;
-          NCCLCHECK(ncclNvlsRegResourcesQuery(comm, info, &recChannels));
+          int recChannels = info->nMaxChannels + 1; // was unset
+          //NCCLCHECK(ncclNvlsRegResourcesQuery(comm, info, &recChannels)); Disabled for topo explorer
           if (recChannels <= info->nMaxChannels) {
             info->algorithm = NCCL_ALGO_NVLS;
             info->protocol = NCCL_PROTO_SIMPLE;
