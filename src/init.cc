06605b5d6a84f073968f66602cdb79ccc381ad02
amd-jiali <jiali.li@amd.com>
2025-10-02 09:32:32 -0700

    Print out the hipRuntimeVersion message from WARN to always show up (#1911)
    Authored-by: Jiali Li <jialili@amd.com>

--- src/init.cc
+++ src/init.cc
@@ -148,7 +148,8 @@ ncclResult_t checkHsaEnvSetting() {
 
   INFO(NCCL_INIT, "Hipruntime version: %d, firmware version: %d", hipRuntimeVersion, firmwareVersion);
   if (!validHsaScratchEnvSetting(hsaScratchEnv, hipRuntimeVersion, firmwareVersion, devProp.gcnArchName)) {
-    WARN("HSA_NO_SCRATCH_RECLAIM=1 must be set to avoid RCCL perf hit, rocm ver:%d", hipRuntimeVersion);
+    // Always print out this warning message
+    printf("Fatal Error: HSA_NO_SCRATCH_RECLAIM=1 must be set to avoid performance degradation with HIP Runtime version:%d, GPU Firmware version:%d\n", hipRuntimeVersion, firmwareVersion);
     return ncclSystemError;
   }
   return ncclSuccess;
