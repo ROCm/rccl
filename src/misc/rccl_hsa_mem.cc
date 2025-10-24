bb5c144cfd6ce43fef03a5395c32fea0f95f5971
PJAvinash <avinashindian2.0@gmail.com>
2025-10-24 16:29:09 +0000

    formatting

--- src/misc/rccl_hsa_mem.cc
+++ src/misc/rccl_hsa_mem.cc
@@ -11,11 +11,9 @@ static const std::vector<hsa_agent_t>& get_all_gpu_agents()
     static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
     static int initialized = 0;
     static std::vector<hsa_agent_t> agents;
-    if (!__atomic_load_n(&initialized, __ATOMIC_ACQUIRE))
-    {
+    if (!__atomic_load_n(&initialized, __ATOMIC_ACQUIRE)) {
         pthread_mutex_lock(&mutex);
-        if (!__atomic_load_n(&initialized, __ATOMIC_RELAXED))
-        {
+        if (!__atomic_load_n(&initialized, __ATOMIC_RELAXED)) {
             hsa_status_t status = hsa_iterate_agents([](hsa_agent_t agent, void *data) -> hsa_status_t
                                {
                             hsa_device_type_t type;
@@ -40,16 +38,12 @@ static const std::unordered_map<uint32_t,hsa_agent_t>& get_bdf_to_gpu_agent_map(
     static int initialized = 0;
     static std::unordered_map<uint32_t,hsa_agent_t> bdf_to_agent_map;
     const std::vector<hsa_agent_t>& gpu_agents = get_all_gpu_agents();
-    if (!__atomic_load_n(&initialized, __ATOMIC_ACQUIRE))
-    {
+    if (!__atomic_load_n(&initialized, __ATOMIC_ACQUIRE)) {
         pthread_mutex_lock(&mutex);
-        if (!__atomic_load_n(&initialized, __ATOMIC_RELAXED))
-        {
-            for (auto &agent : gpu_agents)
-            {
+        if (!__atomic_load_n(&initialized, __ATOMIC_RELAXED)) {
+            for (auto &agent : gpu_agents) {
                 uint32_t bdfid = 0;
-                if (hsa_agent_get_info(agent, (hsa_agent_info_t)HSA_AMD_AGENT_INFO_BDFID, &bdfid) == HSA_STATUS_SUCCESS)
-                {
+                if (hsa_agent_get_info(agent, (hsa_agent_info_t)HSA_AMD_AGENT_INFO_BDFID, &bdfid) == HSA_STATUS_SUCCESS) {
                     bdf_to_agent_map[bdfid] = agent;
                 }
             }
@@ -60,42 +54,35 @@ static const std::unordered_map<uint32_t,hsa_agent_t>& get_bdf_to_gpu_agent_map(
     return bdf_to_agent_map;
 }
 
-static uint32_t pci_bus_str_to_bdfid(const char* bus_id)
-{
+static uint32_t pci_bus_str_to_bdfid(const char* bus_id) {
     unsigned domain, bus, device, function;
-    if (!bus_id || sscanf(bus_id, "%x:%x:%x.%x", &domain, &bus, &device, &function) != 4)
-        return 0xffffffff;
+    if (!bus_id || sscanf(bus_id, "%x:%x:%x.%x", &domain, &bus, &device, &function) != 4) {
+         return 0xffffffff;
+    }
     // Encode domain:bus:device.function as 32-bit BDF ID
     return (domain << 16) | (bus << 8) | (device << 3) | function;
 }
 
-static const std::unordered_map<uint32_t, hsa_agent_t>& get_hip_device_to_hsa_agent_map_by_bdfid()
-{
+static const std::unordered_map<uint32_t, hsa_agent_t>& get_hip_device_to_hsa_agent_map_by_bdfid() {
     static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
     static int initialized = 0;
     static std::unordered_map<uint32_t, hsa_agent_t> device_to_agent;
     // Get HIP device count
-    if (!__atomic_load_n(&initialized, __ATOMIC_ACQUIRE))
-    {
+    if (!__atomic_load_n(&initialized, __ATOMIC_ACQUIRE)) {
         pthread_mutex_lock(&mutex);
-        if (!__atomic_load_n(&initialized, __ATOMIC_RELAXED))
-        {
+        if (!__atomic_load_n(&initialized, __ATOMIC_RELAXED)) {
             int hip_device_count = 0;
             hipError_t devcountStatus = hipGetDeviceCount(&hip_device_count);
             const std::unordered_map<uint32_t, hsa_agent_t> bdf_to_agent = get_bdf_to_gpu_agent_map();
             // Match HIP devices to HSA agents by PCI Bus ID
-            for (uint32_t i = 0; i < hip_device_count; ++i)
-            {
+            for (uint32_t i = 0; i < hip_device_count; ++i) {
                 char busid_str[64];
                 hipError_t pcibusidStatus = hipDeviceGetPCIBusId(busid_str, sizeof(busid_str), i);
                 uint32_t bdfid = pci_bus_str_to_bdfid(busid_str);
                 auto it = bdf_to_agent.find(bdfid);
-                if (it != bdf_to_agent.end())
-                {
+                if (it != bdf_to_agent.end()) {
                     device_to_agent[i] = it->second;
-                }
-                else
-                {
+                } else {
                     // Log warning
                 }
             }
@@ -111,8 +98,8 @@ static hsa_amd_memory_pool_t get_gpu_pool(hsa_agent_t agent) {
     hsa_amd_agent_iterate_memory_pools(agent, [](hsa_amd_memory_pool_t pool, void* data) -> hsa_status_t {
         uint32_t alloc = 0;
         hsa_status_t status = hsa_amd_memory_pool_get_info(pool, HSA_AMD_MEMORY_POOL_INFO_RUNTIME_ALLOC_ALLOWED, &alloc);
-        if (status != HSA_STATUS_SUCCESS && status != HSA_STATUS_INFO_BREAK){
-            // log
+        if (status != HSA_STATUS_SUCCESS && status != HSA_STATUS_INFO_BREAK) {
+            // log warning
         }
         if (alloc) {
             *(hsa_amd_memory_pool_t*)data = pool;
@@ -127,18 +114,15 @@ static hsa_status_t hsa_init_once() {
     static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
     static int initialized = 0;
     static hsa_status_t returnStatus = HSA_STATUS_ERROR_NOT_INITIALIZED;
-    if (!__atomic_load_n(&initialized, __ATOMIC_ACQUIRE))
-    {
+    if (!__atomic_load_n(&initialized, __ATOMIC_ACQUIRE)) {
         pthread_mutex_lock(&mutex);
-        if (!__atomic_load_n(&initialized, __ATOMIC_RELAXED))
-        {
+        if (!__atomic_load_n(&initialized, __ATOMIC_RELAXED)) {
             //returnStatus = hsa_init();
             int hip_device_count = 0;
             hipError_t devcountStatus = hipGetDeviceCount(&hip_device_count);
             if(devcountStatus == hipSuccess){
                 returnStatus = HSA_STATUS_SUCCESS;
             }
-
             __atomic_store_n(&initialized, 1, __ATOMIC_RELEASE);
         }
         pthread_mutex_unlock(&mutex);
@@ -149,8 +133,7 @@ static hsa_status_t hsa_init_once() {
 hipError_t rcclExtMallocWithFlagsOnDevice(void** ptr,
                                          size_t size,
                                          uint32_t flags,
-                                         int device_id)
-{
+                                         int device_id) {
     if (!ptr || size == 0)
         return hipErrorInvalidValue;
 
@@ -165,15 +148,13 @@ hipError_t rcclExtMallocWithFlagsOnDevice(void** ptr,
         return hipErrorInvalidDevice;
     }
     hsa_agent_t target_agent = it->second;
-    // 2. Get the memory pool for that GPU
     hsa_amd_memory_pool_t pool = get_gpu_pool(target_agent);
-    if (pool.handle == 0)
+    if (pool.handle == 0) {
         return hipErrorMemoryAllocation;
-    // 3 Allocate memory
+    }
     void* mem = nullptr;
     status = hsa_amd_memory_pool_allocate(pool, size, flags, &mem);
     CHECK_HSA("hsa_amd_memory_pool_allocate", status);
-    // 4. Allow access for that GPU
     status = hsa_amd_agents_allow_access(1, &target_agent, NULL, mem);
     CHECK_HSA("hsa_amd_agents_allow_access", status);
     *ptr = mem;
@@ -182,8 +163,9 @@ hipError_t rcclExtMallocWithFlagsOnDevice(void** ptr,
 
 
 hipError_t rcclExtFree_impl(void* ptr) {
-    if (!ptr)
+    if (!ptr) {
         return hipErrorInvalidValue;
+    }
     hsa_status_t status = hsa_amd_memory_pool_free(ptr);
     return (status == HSA_STATUS_SUCCESS) ? hipSuccess : hipErrorInvalidDevicePointer;
 }
