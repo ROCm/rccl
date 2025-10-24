dfc972445a19361a2dd30cf84082cf83638b3d50
Corey Derochie <corey.derochie@amd.com>
2025-10-24 13:19:28 -0400

    Fixed improperly initialized array.

--- src/plugin/net.cc
+++ src/plugin/net.cc
@@ -60,7 +60,7 @@ typedef struct netPluginLib {
 
 int pluginCount = 0;
 bool netPluginLibsInitialized = false;
-netPluginLib_t netPluginLibs[NCCL_NET_MAX_PLUGINS] = { 0 };
+netPluginLib_t netPluginLibs[NCCL_NET_MAX_PLUGINS] = {};
 static pthread_mutex_t netPluginLock = PTHREAD_MUTEX_INITIALIZER;
 static pthread_once_t initPluginLibsOnceControl = PTHREAD_ONCE_INIT;
 
