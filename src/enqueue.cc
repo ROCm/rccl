6d8ccd2bdda5510579621b5c9c788b2bc8e5da6c
isaki001 <ioannissakiotis@gmail.com>
2025-10-28 13:57:27 +0000

    enable p2p-batching by default

--- src/enqueue.cc
+++ src/enqueue.cc
@@ -928,7 +928,7 @@ RCCL_PARAM(P2pBatchThreshold, "P2P_BATCH_THRESHOLD", 1 << 16); // 64k
 // Need this temporary parameter to disable p2p batching to avoid some dips at 4MB - 32 MB message size at large scale
 // This parameter must be removed after further investigation,
 // Note that NCCL enables batching by default and it is needed to achieve perf for with smaller messages <= 4MB
-RCCL_PARAM(P2pBatchEnable, "P2P_BATCH_ENABLE", 0); // 64k
+RCCL_PARAM(P2pBatchEnable, "P2P_BATCH_ENABLE", 1); // 64k
 
 // Put p2p op in plan assuming there is sizeof(ncclDevWorkBatch) in batch budget
 // and sizeof(ncclDevWorkP2p) in work budget. "sendRank" and "recvRank" must
