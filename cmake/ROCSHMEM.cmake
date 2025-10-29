95471827bd685f4707bf73f274132bfb9f70bbb3
Thomas Huber <thomas.huber@amd.com>
2025-10-29 16:24:02 +0000

    Adding IPC support in rocSHMEM build

--- cmake/ROCSHMEM.cmake
+++ cmake/ROCSHMEM.cmake
@@ -77,7 +77,7 @@ function(add_rocshmem_targets)
             CONFIGURE_COMMAND   ""
             BUILD_COMMAND
                 ${CMAKE_COMMAND} -E make_directory build
-                && ${CMAKE_COMMAND} -E chdir build bash -lc "../scripts/build_configs/gda_bnxt -DUSE_EXTERNAL_MPI=OFF"
+		&& ${CMAKE_COMMAND} -E chdir build bash -lc "../scripts/build_configs/gda_bnxt -DUSE_EXTERNAL_MPI=OFF -DUSE_IPC=ON "
                 && ${CMAKE_COMMAND} -E chdir build ${CMAKE_COMMAND}
                     -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
                     -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR> ..
