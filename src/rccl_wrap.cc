00c78b702a0ebeb284bef5dd133d108c0ded48ed
isaki001 <ioannissakiotis@gmail.com>
2025-11-07 22:04:38 -0800

    restore 2-node AR simple at earlier message sizes than higher multi-node AR

--- src/rccl_wrap.cc
+++ src/rccl_wrap.cc
@@ -72,7 +72,7 @@ void rcclUpdateCollectiveProtocol(struct ncclComm* comm, size_t const& nBytes, s
       // When LL128 is performant, the next condition overrides the previous LL choice
       if (comm->topo->ll128Enabled) {
         if (info->func == ncclFuncAllReduce) {
-          if(comm->nNodes >= 2) {
+          if(comm->nNodes > 2) {
             ll128Max *= 3.8; // Scale max message size for n > 2 since Tree has special behavior at 2 nodes
           }
           // ll128Max += (log2i(comm->nNodes) - 1) * comm->minMaxLLRange[tunableIndex][NCCL_PROTO_LL128][RCCL_PROTOCOL_FACTOR_IDX];
