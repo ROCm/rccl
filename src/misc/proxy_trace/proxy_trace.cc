b9902c18201273437dd24a686a02b91a87ef2b0a
ahmedkkhan <ahmedkkhan@fb.com>
2025-11-07 17:20:41 -0800

    Format proxy_trace.cc

--- src/misc/proxy_trace/proxy_trace.cc
+++ src/misc/proxy_trace/proxy_trace.cc
@@ -6,10 +6,10 @@
  */
 
 #include "proxy_trace/proxy_trace.h"
+#include <map>
 #include "debug.h"
 #include "device.h"
 #include "proxy.h"
-#include <map>
 
 constexpr int32_t kFinishedProxyOpItems = 32;
 static std::unordered_map<facebook_rccl::ProxyOpStepStatus, std::string>
@@ -31,25 +31,24 @@ void facebook_rccl::ProxyTrace::resetAll() {
   initialized = false;
 }
 
-bool facebook_rccl::ProxyTrace::checkActiveOpExist(uint64_t commHash,
-                                                   uint64_t opCount,
-                                                   uint32_t proxyOpId) const {
-  return (activeOps.find(commHash) != activeOps.end() &&
-          activeOps.at(commHash).find(opCount) !=
-              activeOps.at(commHash).end() &&
-          activeOps.at(commHash).at(opCount).find(proxyOpId) !=
-              activeOps.at(commHash).at(opCount).end());
+bool facebook_rccl::ProxyTrace::checkActiveOpExist(
+    uint64_t commHash, uint64_t opCount, uint32_t proxyOpId) const {
+  return (
+      activeOps.find(commHash) != activeOps.end() &&
+      activeOps.at(commHash).find(opCount) != activeOps.at(commHash).end() &&
+      activeOps.at(commHash).at(opCount).find(proxyOpId) !=
+          activeOps.at(commHash).at(opCount).end());
 }
 
 // Get a unique proxyOpId for a given commHash:opCount
 // If the opCount is not found, create a new entry for it and return 0
-int64_t facebook_rccl::ProxyTrace::getOrCreateProxyOpId(uint64_t commHash,
-                                                        uint64_t opCount) {
+int64_t facebook_rccl::ProxyTrace::getOrCreateProxyOpId(
+    uint64_t commHash, uint64_t opCount) {
   return activeOpIdTracker[commHash][opCount];
 }
 
-facebook_rccl::ProxyTraceOp *
-facebook_rccl::ProxyTrace::getProxyTraceOpPtr(const ProxyTraceRecordKey &key) {
+facebook_rccl::ProxyTraceOp* facebook_rccl::ProxyTrace::getProxyTraceOpPtr(
+    const ProxyTraceRecordKey& key) {
   if (checkActiveOpExist(key.commHash, key.opCount, key.proxyOpId)) {
     return &(activeOps.at(key.commHash).at(key.opCount).at(key.proxyOpId));
   }
@@ -57,9 +56,9 @@ facebook_rccl::ProxyTrace::getProxyTraceOpPtr(const ProxyTraceRecordKey &key) {
 }
 
 void facebook_rccl::ProxyTrace::checkOpCompleted(
-    const ProxyTraceRecordKey &key) {
+    const ProxyTraceRecordKey& key) {
   if (checkActiveOpExist(key.commHash, key.opCount, key.proxyOpId)) {
-    auto &traceOp = activeOps[key.commHash][key.opCount][key.proxyOpId];
+    auto& traceOp = activeOps[key.commHash][key.opCount][key.proxyOpId];
     // Remove finished proxyOp or colls to avoid memory leak
     if (traceOp.counters[facebook_rccl::ProxyCounterTypes::DONE] ==
         traceOp.nSteps) {
@@ -80,8 +79,12 @@ void facebook_rccl::ProxyTrace::checkOpCompleted(
 }
 
 void facebook_rccl::ProxyTrace::addNewProxyTraceOpImpl(
-    const ProxyTraceRecordKey &key, const ProxyTraceExtraInfo &extraInfo,
-    ProxyOpType opType, int channelId, int nSteps, uint32_t nbytes,
+    const ProxyTraceRecordKey& key,
+    const ProxyTraceExtraInfo& extraInfo,
+    ProxyOpType opType,
+    int channelId,
+    int nSteps,
+    uint32_t nbytes,
     int peerRank) {
   if (nSteps > 0 &&
       !checkActiveOpExist(key.commHash, key.opCount, key.proxyOpId)) {
@@ -97,8 +100,8 @@ void facebook_rccl::ProxyTrace::addNewProxyTraceOpImpl(
     traceOp.startTs = std::chrono::high_resolution_clock::now();
     traceOp.status = ProxyOpStepStatus::INIT;
     activeOpIdTracker[key.commHash][key.opCount]++;
-    activeOps[key.commHash][key.opCount].emplace(key.proxyOpId,
-                                                 std::move(traceOp));
+    activeOps[key.commHash][key.opCount].emplace(
+        key.proxyOpId, std::move(traceOp));
   } else if (nSteps == 0) {
     INFO(NCCL_PROXY, "nSteps is 0, ignored %s", key.str().c_str());
   }
@@ -139,16 +142,16 @@ void facebook_rccl::ProxyTraceOp::computeStatus() {
 std::string facebook_rccl::ProxyTrace::dump(uint64_t commHash) {
   std::string result = fmt::format("commDump for commHash:{}\n", commHash);
   std::map<std::string, std::string> sortedDumpStrMap;
-  for (auto &opCountMap : activeOps.at(commHash)) {
-    for (auto &proxyOpMap : opCountMap.second) {
-      ProxyTraceRecordKey traceKey = {commHash, opCountMap.first,
-                                      proxyOpMap.first};
+  for (auto& opCountMap : activeOps.at(commHash)) {
+    for (auto& proxyOpMap : opCountMap.second) {
+      ProxyTraceRecordKey traceKey = {
+          commHash, opCountMap.first, proxyOpMap.first};
       proxyOpMap.second.computeStatus();
       sortedDumpStrMap[traceKey.str()] = proxyOpMap.second.str();
     }
   }
-  for (const auto &pair : sortedDumpStrMap) {
-    result += pair.second; //proxyOpStr
+  for (const auto& pair : sortedDumpStrMap) {
+    result += pair.second; // proxyOpStr
   }
   return result;
 }
@@ -159,10 +162,13 @@ std::string facebook_rccl::ProxyTrace::dump() {
 
   // maps serialized key to serliazed proxyOp; sorted by key
   std::map<std::string, std::string> sortedDumpStrMap;
-  for (auto &commHash_opCountMap : activeOps) {
-    for (auto &opCount_proxyOpMap : commHash_opCountMap.second /*opCountMap*/) {
-      for (auto &opId_opEntry : opCount_proxyOpMap.second/*proxyOpMap*/) {
-        ProxyTraceRecordKey traceKey = {commHash_opCountMap.first, opCount_proxyOpMap.first, opId_opEntry.first};
+  for (auto& commHash_opCountMap : activeOps) {
+    for (auto& opCount_proxyOpMap : commHash_opCountMap.second /*opCountMap*/) {
+      for (auto& opId_opEntry : opCount_proxyOpMap.second /*proxyOpMap*/) {
+        ProxyTraceRecordKey traceKey = {
+            commHash_opCountMap.first,
+            opCount_proxyOpMap.first,
+            opId_opEntry.first};
         opId_opEntry.second.computeStatus();
         sortedDumpStrMap[traceKey.str()] = opId_opEntry.second.str();
       }
@@ -170,10 +176,10 @@ std::string facebook_rccl::ProxyTrace::dump() {
   }
 
   // add the recent finished ops as well
-  for (const auto &keyStr_proxyOpStr : finishedOps) {
+  for (const auto& keyStr_proxyOpStr : finishedOps) {
     sortedDumpStrMap[keyStr_proxyOpStr.first] = keyStr_proxyOpStr.second;
   }
-  for (const auto &keyStr_proxyOpStr : sortedDumpStrMap) {
+  for (const auto& keyStr_proxyOpStr : sortedDumpStrMap) {
     result += keyStr_proxyOpStr.second;
   }
   return result;
@@ -191,9 +197,16 @@ std::string facebook_rccl::ProxyTraceOp::str() {
       std::chrono::duration_cast<std::chrono::milliseconds>(
           lastUpdateTs.time_since_epoch())
           .count(),
-      static_cast<int>(lastUpdatingCounter), traceKey.str(), extraInfo.str(),
-      myRank, peerRank, opType == ProxyOpType::SEND ? "S" : "R", channelId,
-      proxyStepStatusStrMap[status], nSteps, nbytes,
+      static_cast<int>(lastUpdatingCounter),
+      traceKey.str(),
+      extraInfo.str(),
+      myRank,
+      peerRank,
+      opType == ProxyOpType::SEND ? "S" : "R",
+      channelId,
+      proxyStepStatusStrMap[status],
+      nSteps,
+      nbytes,
       counters[ProxyCounterTypes::POSTED],
       counters[ProxyCounterTypes::KERNEL_COPY_READY],
       counters[ProxyCounterTypes::TAIL_OR_HEAD],
@@ -201,42 +214,49 @@ std::string facebook_rccl::ProxyTraceOp::str() {
       counters[ProxyCounterTypes::FIFO_SZ_OR_HEAD_CACHE],
       counters[ProxyCounterTypes::TRANSMITTED],
       counters[ProxyCounterTypes::FLUSHED],
-      counters[ProxyCounterTypes::RECEIVED], counters[ProxyCounterTypes::DONE]);
+      counters[ProxyCounterTypes::RECEIVED],
+      counters[ProxyCounterTypes::DONE]);
   return ret;
 }
 
 float facebook_rccl::ProxyTrace::getMapSizeMB() const {
   float size = 0;
-  for (const auto &commHash_opCountMap : activeOps) {
-    for (const auto &opCount_proxyOpMap : commHash_opCountMap.second) {
+  for (const auto& commHash_opCountMap : activeOps) {
+    for (const auto& opCount_proxyOpMap : commHash_opCountMap.second) {
       size += opCount_proxyOpMap.second.size() *
-              (sizeof(ProxyTraceOp) +
-               sizeof(std::unique_ptr<facebook_rccl::ProxyTraceOp>));
+          (sizeof(ProxyTraceOp) +
+           sizeof(std::unique_ptr<facebook_rccl::ProxyTraceOp>));
     }
   }
-  for (const auto &keyStr_proxyOpStr : finishedOps) {
+  for (const auto& keyStr_proxyOpStr : finishedOps) {
     size += keyStr_proxyOpStr.first.size() + keyStr_proxyOpStr.second.size();
   }
   return size / 1024.0 / 1024.0;
 }
 
-void facebook_rccl::proxyTraceInit(std::unique_ptr<ProxyTrace> &proxyTrace,
-                                   int32_t rank, uint64_t commHash) {
+void facebook_rccl::proxyTraceInit(
+    std::unique_ptr<ProxyTrace>& proxyTrace, int32_t rank, uint64_t commHash) {
   if (proxyTrace) {
-    WARN("[proxyTrace] Initializing non-empty proxyTrace! rank: %d, commHash: "
-         "%lu",
-         rank, commHash);
+    WARN(
+        "[proxyTrace] Initializing non-empty proxyTrace! rank: %d, commHash: "
+        "%lu",
+        rank,
+        commHash);
     return;
   }
-  INFO(NCCL_PROXY, "Initializing ProxyTrace, rank: %d, commHash: %lu", rank,
-       commHash);
+  INFO(
+      NCCL_PROXY,
+      "Initializing ProxyTrace, rank: %d, commHash: %lu",
+      rank,
+      commHash);
   proxyTrace = std::make_unique<facebook_rccl::ProxyTrace>(rank);
   proxyTrace->initialized = true;
 }
 
 void facebook_rccl::updateProxyOpCounter(
-    std::unique_ptr<ProxyTrace> &proxyTraceObj,
-    const ProxyTraceRecordKey &traceKey, ProxyCounterTypes counter,
+    std::unique_ptr<ProxyTrace>& proxyTraceObj,
+    const ProxyTraceRecordKey& traceKey,
+    ProxyCounterTypes counter,
     int64_t val) {
   if (proxyTraceObj) {
     auto traceOpPtr = proxyTraceObj->getProxyTraceOpPtr(traceKey);
@@ -249,15 +269,19 @@ void facebook_rccl::updateProxyOpCounter(
   }
 }
 
-void facebook_rccl::addNewProxyOp(std::unique_ptr<ProxyTrace> &proxyTraceObj,
-                                  ProxyTraceRecordKey &key,
-                                  const ProxyTraceExtraInfo &extraInfo,
-                                  ProxyOpType opType, int channelId, int nSteps,
-                                  uint32_t nbytes, int peerRank) {
+void facebook_rccl::addNewProxyOp(
+    std::unique_ptr<ProxyTrace>& proxyTraceObj,
+    ProxyTraceRecordKey& key,
+    const ProxyTraceExtraInfo& extraInfo,
+    ProxyOpType opType,
+    int channelId,
+    int nSteps,
+    uint32_t nbytes,
+    int peerRank) {
   if (proxyTraceObj) {
     auto opId = proxyTraceObj->getOrCreateProxyOpId(key.commHash, key.opCount);
     key.proxyOpId = opId;
-    proxyTraceObj->addNewProxyTraceOpImpl(key, extraInfo, opType, channelId,
-                                          nSteps, nbytes, peerRank);
+    proxyTraceObj->addNewProxyTraceOpImpl(
+        key, extraInfo, opType, channelId, nSteps, nbytes, peerRank);
   }
 }
