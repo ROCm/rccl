4eaf368a3b2076ee8459ba441f92f793dccbc6a7
Ghadeer Ahmed H Alabandi <abandiga2@gmail.com>
2025-11-13 11:05:41 -0800

    disable pipelining for gfx950 in rcclSetPipelining

--- src/rccl_wrap.cc
+++ src/rccl_wrap.cc
@@ -239,7 +239,7 @@ void rcclUpdateThreadThreshold(struct ncclComm* comm, size_t const& nBytes, stru
 
 void rcclSetPipelining(struct ncclComm* comm, size_t const& nBytes, struct ncclTaskColl* info) {
   info->pipeline = 0; // Default to no pipelining
-  if (rcclParamdisableReduceCopyPipelining()) {
+  if (rcclParamdisableReduceCopyPipelining() || IsArchMatch(comm->topo->nodes[GPU].nodes[0].gpu.gcn, "gfx950")) {
     return;
   }
   const bool dtypeOK = (info->datatype == ncclBfloat16) || rcclParamPipelineAllDTypes();
