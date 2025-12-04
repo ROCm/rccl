53726db99f3d14de94aca2c47b735d8a5c91a328
Thomas Huber <thomas.huber@amd.com>
2025-12-04 15:54:34 +0000

    Updating to no-ctx wg call and updating submodule

--- src/device/alltoall_gda.h
+++ src/device/alltoall_gda.h
@@ -17,22 +17,18 @@ struct RunWorkColl<ncclFuncAllToAllGda, T, RedOp, NCCL_ALGO_RING, NCCL_PROTO_SIM
   __device__ __forceinline__ void run(int tid, int nThreads, struct ncclDevWorkColl* work) {
     using Proto = ProtoSimple<ALLTOALL_PIVOT_CHUNKSTEPS/ALLTOALL_PIVOT_SLICESTEPS, ALLTOALL_PIVOT_SLICESTEPS>;
 	if (blockIdx.x == 0) {
-           __shared__ rocshmem::rocshmem_ctx_t ctx;
-           int64_t ctx_type = 0;
+         
 
-           rocshmem::rocshmem_wg_ctx_create(ctx_type, &ctx);
-           int num_pes = rocshmem::rocshmem_ctx_n_pes(ctx);
+    int num_pes = rocshmem::rocshmem_n_pes();
 
 	   reduceCopy<COLL_UNROLL, USE_ACC, RedOp, T, 0,1, 1, 0, 1, 1, 0>(
               tid, nThreads, 0, nullptr, false, 1, (void **)&work->sendbuff, 1, (void **)&work->sndbuff, 
 	      (work->size*num_pes));
 
-           rocshmem_ctx_char_alltoall_wg(ctx, work->team, ((char*)work->tempbuff), ((char*)work->sndbuff), work->size);
+           rocshmem::rocshmem_char_alltoall_wg(work->team, ((char*)work->tempbuff), ((char*)work->sndbuff), work->size);
 
            //rocshmem_ctx_quiet(ctx);
            //__syncthreads();
-
-           rocshmem_wg_ctx_destroy(&ctx);
 
            reduceCopy<COLL_UNROLL, USE_ACC, RedOp, T, 0,1, 1, 0, 1, 1, 0>(
               tid, nThreads, 0, nullptr, false, 1, (void **)&work->tempbuff, 1, (void **)&work->recvbuff, 
