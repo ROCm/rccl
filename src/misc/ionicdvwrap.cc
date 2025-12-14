13d41f101b3bec3b3fb8a2d0a3a3d5c32da98322
Karthikeyan Arumugam <karthik@pensando.io>
2025-12-14 08:11:25 +0000

    Fix typo with usage of rccl param api RCCL_AINIC_ROCE

--- src/misc/ionicdvwrap.cc
+++ src/misc/ionicdvwrap.cc
@@ -10,11 +10,11 @@ static pthread_once_t initOnceControl = PTHREAD_ONCE_INIT;
 static ncclResult_t initResult;
 struct ncclIonicdvSymbols ionicdvSymbols;
 
-extern int64_t rcclParamAinic();
+extern int64_t rcclParamAinicRoce();
 
 ncclResult_t wrap_ionicdv_symbols(void) {
 #if defined(__HIP_PLATFORM_AMD__) || defined(__HIPCC__)
-  if (rcclParamAinic() == 1) {
+  if (rcclParamAinicRoce() == 1) {
     pthread_once(&initOnceControl,
                  [](){ initResult = buildIonicdvSymbols(&ionicdvSymbols); });
     return initResult;
