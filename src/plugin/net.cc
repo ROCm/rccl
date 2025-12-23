c3831149004c8517d64c0937ae6c825e2e9e0b7e
Karthikeyan Arumugam <karthik@pensando.io>
2025-12-23 09:41:46 +0000

    Updated plugin load function to load default internal net-ib only when its not AINIC and External plugin env is not set

--- src/plugin/net.cc
+++ src/plugin/net.cc
@@ -38,11 +38,7 @@ int ncclNetVersion[NCCL_NET_VERSION_COUNT] = {10, 9, 8, 7, 6};
 getNcclNet_t* getNcclNet[NCCL_NET_VERSION_COUNT] = {getNcclNet_v10, getNcclNet_v9, getNcclNet_v8, getNcclNet_v7, getNcclNet_v6};
 getNcclCollNet_t* getNcclCollNet[NCCL_NET_VERSION_COUNT] = {getNcclCollNet_v10, getNcclCollNet_v9, getNcclCollNet_v8, getNcclCollNet_v7,  getNcclCollNet_v6};
 
-#if defined(__HIP_PLATFORM_AMD__) || defined(__HIPCC__)
-#define NCCL_NET_NUM_INTERNAL_PLUGINS 3
-#else
 #define NCCL_NET_NUM_INTERNAL_PLUGINS 2
-#endif
 
 typedef enum ncclNetPluginState {
   ncclNetPluginStateDisabled        = -2,       // Plugin library failed to initialize
@@ -249,16 +245,19 @@ static void initPluginLibsOnceFunc() {
     strcpy(netPluginLibs[pluginCounter++].name, defaultNetPlugin);
   }
 
+  // Add 2 internal ib and socket plugins
 #if defined(__HIP_PLATFORM_AMD__) || defined(__HIPCC__)
-  // Add rocm internal ib and internal socket plugins
   if ((rcclParamAinicRoce() == 1) && !(envNetPlugin)) {
+    // For AINIC add rocm internal ib instead of default internal ib
     netPluginLibs[pluginCounter].ncclNet = &rocmNetIb;
     netPluginLibs[pluginCounter++].ncclNetPluginState = ncclNetPluginStateInitReady;
+  } else {
+#endif
+    netPluginLibs[pluginCounter].ncclNet = &ncclNetIb;
+    netPluginLibs[pluginCounter++].ncclNetPluginState = ncclNetPluginStateInitReady;
+#if defined(__HIP_PLATFORM_AMD__) || defined(__HIPCC__)
   }
 #endif
-  // Add 2 internal ib and socket plugins
-  netPluginLibs[pluginCounter].ncclNet = &ncclNetIb;
-  netPluginLibs[pluginCounter++].ncclNetPluginState = ncclNetPluginStateInitReady;
   netPluginLibs[pluginCounter].ncclNet = &ncclNetSocket;
   netPluginLibs[pluginCounter++].ncclNetPluginState = ncclNetPluginStateInitReady;
   pluginCount = pluginCounter;
