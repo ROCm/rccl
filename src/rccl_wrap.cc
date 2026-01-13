093162ec0b05a174247dbc5d7c7ebdea7d96c77e
Mustafa Abduljabbar <muabdulj@amd.com>
2026-01-13 23:16:53 +0000

    Make sure user's maxNchannels is respected

--- src/rccl_wrap.cc
+++ src/rccl_wrap.cc
@@ -164,15 +164,15 @@ extern int64_t ncclParamMaxNchannels();
 RCCL_PARAM(ChannelTuningEnable, "CHANNEL_TUNING_ENABLE", 1);
 
 ncclResult_t rcclOverrideChannels(struct ncclComm* comm, ncclFunc_t coll, size_t nBytes, int& nc){
-#ifdef ENABLE_WARP_SPEED
-  if(ncclParamMaxNchannels() > 0) {
-    nc = std::min(nc, static_cast<int>(ncclParamMaxNchannels()));
-  }
-#endif
+// #ifdef ENABLE_WARP_SPEED
+//   if(ncclParamMaxNchannels() > 0) {
+//     nc = std::min(nc, );
+//   }
+// #endif
   if(comm->nNodes < 2) {
 #ifdef ENABLE_WARP_SPEED
     if(IsArchMatch(comm->topo->nodes[GPU].nodes[0].gpu.gcn, "gfx950")) {
-      nc = std::min(nc, 56); // limit to 56 channels for single node on MI350X
+      nc = std::min(nc, (ncclParamMaxNchannels() > 0)? static_cast<int>(ncclParamMaxNchannels()) : 56); // limit to 56 channels for single node on MI350X
     }
 #endif
     if(!rcclParamChannelTuningEnable()) {
@@ -220,7 +220,8 @@ ncclResult_t rcclOverrideChannels(struct ncclComm* comm, ncclFunc_t coll, size_t
   }
 #ifdef ENABLE_WARP_SPEED
   // Fall back to max 56/64 channels and tune warp speed channels later
-  nc = std::min(nc, (comm->nNodes == 1)? 56 : 64);
+  int maxNchannels = (ncclParamMaxNchannels() > 0)? static_cast<int>(ncclParamMaxNchannels()) : ((comm->nNodes == 1)? 56 : 64);
+  nc = std::min(nc, maxNchannels);
 #endif
   return ncclSuccess;
 }
