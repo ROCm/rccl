1527bef04349302830e2bdb58cec19f8a37cd6f0
Ghadeer Alabandi <abandiga@gmail.com>
2026-01-16 21:33:28 +0000

    Add Copyright

--- src/device/reduce_scatter_direct.h
+++ src/device/reduce_scatter_direct.h
@@ -1,3 +1,9 @@
+/*************************************************************************
+ * Copyright (c) 2015-2022, NVIDIA CORPORATION. All rights reserved.
+ * Modifications Copyright (c) 2019-2022 Advanced Micro Devices, Inc. All rights reserved.
+ *
+ * See LICENSE.txt for license information
+ ************************************************************************/
 
 #include "device.h"
 #include "collectives.h"
